44a9f5cfae09adb18cb6ecc8784ac9a3c346f61c
jlcaron <jlcaron@google.com>
2019-08-18 14:18:31 -0600

    fix tests

--- cdm-test/src/test/java/ucar/nc2/ft/point/standard/TestTableAnalyzer.java
+++ cdm-test/src/test/java/ucar/nc2/ft/point/standard/TestTableAnalyzer.java
@@ -19,22 +19,11 @@ public class TestTableAnalyzer {
 
   @Test
   public void testStuff() throws IOException {
-    /* doit("C:/data/dt2/station/Surface_METAR_20080205_0000.nc");
-    doit("C:/data/bufr/edition3/idd/profiler/PROFILER_3.bufr");
-    doit("C:/data/bufr/edition3/idd/profiler/PROFILER_2.bufr");
-    doit("C:/data/profile/PROFILER_wind_01hr_20080410_2300.nc"); */
-    //doit("C:/data/test/20070301.nc");
-    //doit("C:/data/dt2/profile/PROFILER_3.bufr");
+    doit("ncss/point_features/metars/Surface_METAR_20130823_0000.nc");
+    doit("ncss/point_features/windProfilers/20131101/PROFILER_wind_06min_20131101_2348.nc");
 
-    //doit("C:/data/dt2/station/ndbc.nc");
-    //doit("C:/data/dt2/station/madis2.sao");
-    // doit("C:/data/metars/Surface_METAR_20070326_0000.nc");  // ok
-    //doit("C:/data/dt2/station/Sean_multidim_20070301.nc"); // ok
-    //doit("Q:/cdmUnitTest/formats/gempak/surface/20090521_sao.gem");
-    doit("datasets/metars/Surface_METAR_20070513_0000.nc");
-
-    //doit("C:/data/profile/PROFILER_wind_01hr_20080410_2300.nc");
-    //doit("C:/data/cadis/tempting");
+    // LOOK add all in directory
+    // doAll("cfPoint/");
   }
 
 
