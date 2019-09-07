9ee39ca9d5cebbab491fa6574b0e43b3774d12d4
jlcaron <jlcaron@google.com>
2019-09-07 17:27:55 -0600

    spotless

--- cdm-test/src/test/java/ucar/nc2/dt/image/TestImageDatasetFactory.java
+++ cdm-test/src/test/java/ucar/nc2/dt/image/TestImageDatasetFactory.java
@@ -16,22 +16,22 @@ import ucar.unidata.util.test.category.NeedsCdmUnitTest;
 
 @Category(NeedsCdmUnitTest.class)
 public class TestImageDatasetFactory {
-    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    public byte[] convert(String srcPath) throws IOException {
-      try (GridDataset gds = GridDataset.open(srcPath)) {
-        GeoGrid grid = gds.findGridByName("Pressure_surface");
-        ImageDatasetFactory factory = new ImageDatasetFactory();
-        BufferedImage image = factory.openDataset(grid);
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        ImageIO.write(image, "png", os);
-        return os.toByteArray();
-      }
+  public byte[] convert(String srcPath) throws IOException {
+    try (GridDataset gds = GridDataset.open(srcPath)) {
+      GeoGrid grid = gds.findGridByName("Pressure_surface");
+      ImageDatasetFactory factory = new ImageDatasetFactory();
+      BufferedImage image = factory.openDataset(grid);
+      ByteArrayOutputStream os = new ByteArrayOutputStream();
+      ImageIO.write(image, "png", os);
+      return os.toByteArray();
     }
+  }
 
-    @Test
-    public void testStuff() throws IOException {
-      convert(TestDir.cdmUnitTestDir + "ft/grid/GFS_Global_onedeg_20081229_1800.grib2.nc");
-    }
+  @Test
+  public void testStuff() throws IOException {
+    convert(TestDir.cdmUnitTestDir + "ft/grid/GFS_Global_onedeg_20081229_1800.grib2.nc");
+  }
 
 }
