54a554c1d2a32e8eb939b8ce7ce4c3a064b4e8fa
Ryan May <rmay@ucar.edu>
2019-11-20 02:17:27 -0700

    Refactor NEXRAD level2 format checking
    Don't try to enumerate all the possible values. Instead, just look for
    AR2V and a number--and log a warning is this number is bigger than the
    most recent we are aware of. Also refactor to eliminate duplicated code.

--- cdm/radial/src/main/java/ucar/nc2/iosp/nexrad2/Nexrad2IOServiceProvider.java
+++ cdm/radial/src/main/java/ucar/nc2/iosp/nexrad2/Nexrad2IOServiceProvider.java
@@ -35,15 +35,19 @@ public class Nexrad2IOServiceProvider extends AbstractIOServiceProvider {
   private static final int MISSING_INT = -9999;
   private static final float MISSING_FLOAT = Float.NaN;
 
+  static public boolean isNEXRAD2Format(String format) {
+    if (format != null && (format.equals("ARCHIVE2") || format.startsWith("AR2V"))) {
+      if (Integer.parseInt(format.substring(4)) > 8)
+        logger.warn("Trying to handle unknown but valid-looking format: " + format);
+      return true;
+    }
+    return false;
+  }
 
   public boolean isValidFile(RandomAccessFile raf) {
     try {
       raf.seek(0);
-      String test = raf.readString(8);
-      return test.equals(Level2VolumeScan.ARCHIVE2) || test.equals(Level2VolumeScan.AR2V0001)
-          || test.equals(Level2VolumeScan.AR2V0003) || test.equals(Level2VolumeScan.AR2V0004)
-          || test.equals(Level2VolumeScan.AR2V0002) || test.equals(Level2VolumeScan.AR2V0006)
-          || test.equals(Level2VolumeScan.AR2V0007);
+      return isNEXRAD2Format(raf.readString(8));
     } catch (IOException ioe) {
       return false;
     }
