04b63cd160a23cb6a554682eb9bcc53a4a67f0a8
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2020-04-06 18:00:28 -0600

    Tweak test categories
    Allow Slow, RDA to be set on the command line
    Create new catetory for tests that need access to the UCAR network

--- testUtil/src/main/java/ucar/unidata/util/test/category/Slow.java
+++ testUtil/src/main/java/ucar/unidata/util/test/category/Slow.java
@@ -4,8 +4,12 @@ package ucar.unidata.util.test.category;
  * A marker to be used with JUnit categories to indicate that a test method or test class takes a long time to run.
  * We'll want to avoid running such tests after every commit; instead just run them once-a-night or so.
  *
- * @author cwardgar
- * @since 2015/09/22
+ * To enable these tests, set the runSlowTests system property, for example
+ *
+ * ./gradlew -DrunSlowTests=true :subproject:test
+ *
+ * See gradle/root/testing.gradle
+ *
  */
 public interface Slow {
 }
