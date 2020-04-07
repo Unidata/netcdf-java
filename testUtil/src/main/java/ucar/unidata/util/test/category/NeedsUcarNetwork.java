b2d95e8858db48e4c2678bb5f13735005b92054e
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2020-04-06 18:00:28 -0600

    Tweak test categories
    Allow Slow, RDA to be set on the command line
    Create new catetory for tests that need access to the UCAR network
    
    (cherry picked from commit 04b63cd160a23cb6a554682eb9bcc53a4a67f0a8)

---
+++ testUtil/src/main/java/ucar/unidata/util/test/category/NeedsUcarNetwork.java
@@ -0,0 +1,17 @@
+package ucar.unidata.util.test.category;
+
+
+/**
+ * A marker to be used with JUnit categories to indicate that a test method or test class requires access to a
+ * resource that is only accessible on the UCAR network in order to complete successfully.
+ *
+ * To enable these tests, set the ucarNetworkAvailable system property, for example
+ *
+ * ./gradlew -DucarNetworkAvailable=true :cdm:cdm-s3:test --tests ucar.unidata.io.s3.TestS3Read
+ *
+ * See gradle/root/testing.gradle
+ *
+ */
+public interface NeedsUcarNetwork {
+
+}
