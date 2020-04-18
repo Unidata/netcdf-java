34b3d75a1cc4ded71d7198c9161a690a1531b359
John Caron <jcaron1129@gmail.com>
2020-04-18 17:45:42 -0600

    sequenceDS (incomplete)

--- cdm/core/src/main/java/ucar/nc2/dataset/SequenceDS.java
+++ cdm/core/src/main/java/ucar/nc2/dataset/SequenceDS.java
@@ -5,14 +5,20 @@
 
 package ucar.nc2.dataset;
 
+import ucar.ma2.DataType;
+import ucar.nc2.Attribute;
 import ucar.nc2.Group;
 import ucar.ma2.StructureDataIterator;
 import ucar.ma2.StructureData;
 import ucar.ma2.Array;
 import java.io.IOException;
+import ucar.nc2.Sequence;
+import ucar.nc2.Structure;
+import ucar.nc2.Variable;
+import ucar.nc2.constants.CDM;
 
 /**
- * Enhance sequence
+ * Enhanced sequence
  * 
  * @author caron
  * @since Nov 10, 2009
@@ -20,8 +26,6 @@ import java.io.IOException;
  */
 @Deprecated
 public class SequenceDS extends StructureDS {
-  private ucar.nc2.Sequence orgSeq;
-
   public SequenceDS(Group g, ucar.nc2.Sequence orgSeq) {
     super(g, orgSeq);
     this.orgSeq = orgSeq;
@@ -83,6 +87,98 @@ public class SequenceDS extends StructureDS {
   public Array read() throws IOException {
     Array data = orgSeq.read();
     return convert(data, null);
+  }
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  private ucar.nc2.Sequence orgSeq;
+
+  protected SequenceDS(Builder<?> builder, Group parentGroup) {
+    super(builder, parentGroup);
+    this.orgSeq = builder.orgSeq;
+    this.orgVar = builder.orgSeq;
+    this.orgName = builder.orgName;
+  }
+
+  @Override
+  public Builder<?> toBuilder() {
+    return addLocalFieldsToBuilder(builder());
+  }
+
+  // Add local fields to the passed - in builder.
+  protected Builder<?> addLocalFieldsToBuilder(Builder<? extends Builder<?>> b) {
+    b.setOriginalVariable(this.orgVar).setOriginalName(this.orgName).setUnits(this.proxy.units)
+        .setDesc(this.proxy.desc);
+    return (Builder<?>) super.addLocalFieldsToBuilder(b);
+  }
+
+  public static Builder<?> builder() {
+    return new Builder2();
+  }
+
+  private static class Builder2 extends Builder<Builder2> {
+    @Override
+    protected Builder2 self() {
+      return this;
+    }
+  }
+
+  public static abstract class Builder<T extends Builder<T>> extends StructureDS.Builder<T> {
+    private ucar.nc2.Sequence orgSeq;
+    private boolean built;
+
+    public T setOriginalVariable(Sequence orgVar) {
+      this.orgSeq = orgVar;
+      return self();
+    }
+
+    public T setOriginalName(String orgName) {
+      this.orgName = orgName;
+      return self();
+    }
+
+    public T setUnits(String units) {
+      this.units = units;
+      if (units != null) {
+        addAttribute(new Attribute(CDM.UNITS, units));
+      }
+      return self();
+    }
+
+    public T setDesc(String desc) {
+      this.desc = desc;
+      if (desc != null) {
+        addAttribute(new Attribute(CDM.LONG_NAME, desc));
+      }
+      return self();
+    }
+
+    /** Copy metadata from orgVar. */
+    public T copyFrom(Sequence orgVar) {
+      super.copyFrom(orgVar);
+      for (Variable v : orgVar.getVariables()) {
+        Variable.Builder newVar;
+        if (v instanceof Sequence) {
+          newVar = SequenceDS.builder().copyFrom((Sequence) v);
+        } else if (v instanceof Structure) {
+          newVar = StructureDS.builder().copyFrom((Structure) v);
+        } else {
+          newVar = VariableDS.builder().copyFrom(v);
+        }
+        addMemberVariable(newVar);
+      }
+      setOriginalVariable(orgVar);
+      setOriginalName(orgVar.getShortName());
+      return self();
+    }
+
+    /** Normally this is called by Group.build() */
+    public SequenceDS build(Group parentGroup) {
+      if (built)
+        throw new IllegalStateException("already built");
+      built = true;
+      this.setDataType(DataType.SEQUENCE);
+      return new SequenceDS(this, parentGroup);
+    }
   }
 
 }
