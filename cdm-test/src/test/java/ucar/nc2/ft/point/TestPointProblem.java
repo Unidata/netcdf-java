0f592a79380973d5dc427e2446889d2c56b33d0f
John Caron <jcaron1129@gmail.com>
2020-07-22 14:57:50 -0600

    NeedsCdmUnitTest

--- cdm-test/src/test/java/ucar/nc2/ft/point/TestPointProblem.java
+++ cdm-test/src/test/java/ucar/nc2/ft/point/TestPointProblem.java
@@ -6,17 +6,18 @@ package ucar.nc2.ft.point;
 
 import java.io.IOException;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import ucar.nc2.constants.FeatureType;
 import ucar.unidata.util.test.CheckPointFeatureDataset;
 import ucar.unidata.util.test.TestDir;
+import ucar.unidata.util.test.category.NeedsCdmUnitTest;
 
 /** Run specific problem. */
+@Category(NeedsCdmUnitTest.class)
 public class TestPointProblem {
 
   @Test
-  @Ignore
   public void checkPointDataset() throws IOException {
     String location = TestDir.cdmUnitTestDir + "cfPoint/point/filtered_apriori_super_calibrated_binned1.nc";
     FeatureType ftype = FeatureType.ANY_POINT;
