75d6765eb2eec7015485a81ddffe598fc013faa0
John Caron <jcaron1129@gmail.com>
2020-08-15 09:43:41 -0600

    Add @Category(NeedsCdmUnitTest.class) to new unit test

--- cdm-test/src/test/java/ucar/nc2/dt/grid/TestReadProblem.java
+++ cdm-test/src/test/java/ucar/nc2/dt/grid/TestReadProblem.java
@@ -5,12 +5,15 @@
 package ucar.nc2.dt.grid;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import ucar.unidata.util.test.TestDir;
+import ucar.unidata.util.test.category.NeedsCdmUnitTest;
 
 /** Open specific problem dataset with GridDataset.open(). */
 public class TestReadProblem {
 
   @Test
+  @Category(NeedsCdmUnitTest.class)
   public void doOne() throws Exception {
     TestReadandCount.doOne(TestDir.cdmUnitTestDir + "formats/grib2/", "eta218.wmo", 57, 16, 20, 11);
   }
