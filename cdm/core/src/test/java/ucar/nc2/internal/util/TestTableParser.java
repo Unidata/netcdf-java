62dfe5e5923175f193591d8ab56e15f9bd15c023
John Caron <jcaron1129@gmail.com>
2020-08-18 16:31:08 -0600

    TableParser test.

--- cdm/core/src/test/java/ucar/nc2/internal/util/TestTableParser.java
+++ cdm/core/src/test/java/ucar/nc2/internal/util/TestTableParser.java
@@ -5,6 +5,8 @@
 
 package ucar.nc2.internal.util;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -13,24 +15,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
 import java.util.List;
-import ucar.nc2.internal.util.TableParser;
+import ucar.unidata.util.test.TestDir;
 
-/**
- * Test TableParser.readTable
- *
- * @author caron
- * @since 4/19/12
- */
+/** Test {@link ucar.nc2.internal.util.TableParser} */
 public class TestTableParser {
   private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   ////////////////////////////////////////////////////////////////////////////////////
-  static final String testName3 = "/resources/nj22/tables/nexrad.tbl";
+  static final String fromResource = "/resources/nj22/tables/nexrad.tbl";
+  static final String fromFile = TestDir.cdmLocalTestDataDir + "kma-ver5.txt";
 
   @Test
-  public void testReadNexradTable() throws IOException {
-    Class c = TableParser.class;
-    InputStream is = c.getResourceAsStream(testName3);
+  public void testReadNexradTableAsStream() throws IOException {
+    Class<?> c = TableParser.class;
+    InputStream is = c.getResourceAsStream(fromResource);
     List<TableParser.Record> recs = TableParser.readTable(is, "3,15,54,60d,67d,73d", 50000);
 
     TableParser.Record rec = recs.get(0);
@@ -48,6 +46,18 @@ public class TestTableParser {
     Assert.assertEquals(3898.0, (Double) rec.get(3), 0.1);
     Assert.assertEquals(-9622.0, (Double) rec.get(4), 0.1);
     Assert.assertEquals(417.0, (Double) rec.get(5), 0.1);
+  }
+
+  @Test
+  public void testReadNexradTable() throws IOException {
+    List<TableParser.Record> recs = TableParser.readTable(fromFile, "41,112,124i,136i,148i,160", 50000);
+
+    TableParser.Record rec = recs.get(0);
+    assertThat(rec.get(0).toString().trim()).isEqualTo("U_COMPNT_OF_WIND_AFTER_TIMESTEP");
+    assertThat(rec.get(2)).isEqualTo(0);
+    assertThat(rec.get(3)).isEqualTo(2);
+    assertThat(rec.get(4)).isEqualTo(2);
+    assertThat(rec.get(5).toString().trim()).isEqualTo("m/s");
   }
 
 }
