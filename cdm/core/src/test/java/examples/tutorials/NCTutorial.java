3d029fc32274b544895c7525a5f55038e5aefe6c
haileyajohnson <hailey.johnson@ufl.edu>
2020-08-27 14:58:26 -0600

    fix style; find last (not first) occurence of hime directory in working path

--- cdm/core/src/test/java/examples/tutorials/NCTutorial.java
+++ cdm/core/src/test/java/examples/tutorials/NCTutorial.java
@@ -5,28 +5,28 @@ import ucar.nc2.NetcdfFiles;
 import java.io.IOException;
 
 public class NCTutorial {
-    // logs error/info message in memory and can be accessed from test functions
-    static TestLogger logger = TestLogger.TestLoggerFactory.getLogger();
+  // logs error/info message in memory and can be accessed from test functions
+  static TestLogger logger = TestLogger.TestLoggerFactory.getLogger();
 
-    ////////////////////////////
-    // constant string messages for tests
-    static final String yourOpenNetCdfFileErrorMsgTxt = "exception while opening Netcdf file";
+  ////////////////////////////
+  // constant string messages for tests
+  static final String yourOpenNetCdfFileErrorMsgTxt = "exception while opening Netcdf file";
 
-    ////////////////////////////
-    // Netcdf tutorial functions
-    // NOTE: these functions are used in the NetCdf tutorial docs, so formatting matters!
+  ////////////////////////////
+  // Netcdf tutorial functions
+  // NOTE: these functions are used in the NetCdf tutorial docs, so formatting matters!
 
-    /**
-     * Code snippet to open a netcdf file and log exceptions
-     *
-     * @param pathToYourFileAsStr: relative path to locally stored file
-     */
-    public static void openNCFileTutorial(String pathToYourFileAsStr) {
-        try (NetcdfFile ncfile = NetcdfFiles.open(pathToYourFileAsStr)) {
-            // Do cool stuff here
-        } catch (IOException ioe) {
-            // Handle less-cool exceptions here
-            logger.log(yourOpenNetCdfFileErrorMsgTxt, ioe);
-        }
+  /**
+   * Code snippet to open a netcdf file and log exceptions
+   *
+   * @param pathToYourFileAsStr: relative path to locally stored file
+   */
+  public static void openNCFileTutorial(String pathToYourFileAsStr) {
+    try (NetcdfFile ncfile = NetcdfFiles.open(pathToYourFileAsStr)) {
+      // Do cool stuff here
+    } catch (IOException ioe) {
+      // Handle less-cool exceptions here
+      logger.log(yourOpenNetCdfFileErrorMsgTxt, ioe);
     }
+  }
 }
