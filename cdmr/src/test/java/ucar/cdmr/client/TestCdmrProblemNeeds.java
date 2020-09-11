ead95c8b9f082218c9d07df5861205deb2ea21fc
John Caron <jcaron1129@gmail.com>
2020-09-10 18:07:17 -0600

    Fixes to jenkins testing.

--- cdmr/src/test/java/ucar/cdmr/client/TestCdmrProblemNeeds.java
+++ cdmr/src/test/java/ucar/cdmr/client/TestCdmrProblemNeeds.java
@@ -15,9 +15,10 @@ import ucar.nc2.internal.util.CompareNetcdf2;
 import ucar.unidata.util.test.TestDir;
 import ucar.unidata.util.test.category.NeedsCdmUnitTest;
 import ucar.unidata.util.test.category.NeedsExternalResource;
+import ucar.unidata.util.test.category.NotJenkins;
 
 /** Test {@link CdmrNetcdfFile} */
-@Category({NeedsExternalResource.class, NeedsCdmUnitTest.class}) // Needs CmdrServer to be started up
+@Category({NotJenkins.class, NeedsCdmUnitTest.class}) // Needs CmdrServer to be started up
 public class TestCdmrProblemNeeds {
 
   private final String filename;
