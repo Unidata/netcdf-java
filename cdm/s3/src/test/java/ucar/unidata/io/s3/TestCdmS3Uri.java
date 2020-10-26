7b39dc417cc911ec4053c0277ca14a3f27f76dc4
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2020-10-26 08:52:55 -0600

    Extend CdmS3Uri to include the concept of a delimiter
    A delimiter can be used to indicate that there is a hierarchical
    structure in the keys of a bucket. For example, if files are uploaded to
    an object store and are given a key name equal to their path on disk,
    the delimiter would likely be "/" or "\". The extension is made using
    the addition of a URI fragment, something like `#delimiter="/"`

--- cdm/s3/src/test/java/ucar/unidata/io/s3/TestCdmS3Uri.java
+++ cdm/s3/src/test/java/ucar/unidata/io/s3/TestCdmS3Uri.java
@@ -1,6 +1,12 @@
+/*
+ * Copyright (c) 2019-2020 University Corporation for Atmospheric Research/Unidata
+ * See LICENSE for license information.
+ */
+
 package ucar.unidata.io.s3;
 
 import static com.google.common.truth.Truth.assertThat;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Optional;
@@ -17,6 +23,7 @@ public class TestCdmS3Uri {
   private static final String endpoint_segments = "endpoint/path/";
   private static final String bucket_name = "bucket-name";
   private static final String query = "super/long/key";
+  private static final String fragment = "delimiter=/";
 
   private static final String schemeHttp = "http";
   private static final String schemeHttps = "https";
@@ -24,15 +31,57 @@ public class TestCdmS3Uri {
 
   @Test
   public void testAwsMinimumDep() throws URISyntaxException {
-    // cdms3:/bucket/key
+    // s3://bucket/key
     String cdmS3Uri = "s3://" + bucket_name + "/" + query;
     testUri(cdmS3Uri);
+  }
+
+  @Test
+  public void testAwsDepWithConfig() throws URISyntaxException {
+    // s3://bucket/key#config
+    String cdmS3Uri = "s3://" + bucket_name + "/" + query + "#" + fragment;
+    testUri(cdmS3Uri);
+  }
+
+  @Test
+  public void testAwsBucketOnly() throws URISyntaxException {
+    CdmS3Uri cdmS3Uri = new CdmS3Uri(schemeCdmS3 + "://" + user_info + "@aws" + "/" + bucket_name);
+    assertThat(cdmS3Uri.getBucket()).isEqualTo(bucket_name);
+  }
+
+  @Test
+  public void testAwsAuthorityOnly() throws URISyntaxException {
+    CdmS3Uri cdmS3Uri = new CdmS3Uri(schemeCdmS3 + ":" + bucket_name);
+    assertThat(cdmS3Uri.getBucket()).isEqualTo(bucket_name);
+  }
+
+  @Test
+  public void testHostBucketOnly() throws URISyntaxException {
+    CdmS3Uri cdmS3Uri = new CdmS3Uri(schemeCdmS3 + "://" + host + "/" + bucket_name);
+    assertThat(cdmS3Uri.getBucket()).isEqualTo(bucket_name);
+    cdmS3Uri = new CdmS3Uri(schemeCdmS3 + "://" + host + ":8080/" + bucket_name);
+    assertThat(cdmS3Uri.getBucket()).isEqualTo(bucket_name);
+  }
+
+  @Test
+  public void testHostWithPathBucketOnly() throws URISyntaxException {
+    CdmS3Uri cdmS3Uri = new CdmS3Uri(schemeCdmS3 + "://" + host + "/" + bucket_name);
+    assertThat(cdmS3Uri.getBucket()).isEqualTo(bucket_name);
+    cdmS3Uri = new CdmS3Uri(schemeCdmS3 + "://" + host + ":8080/" + bucket_name);
+    assertThat(cdmS3Uri.getBucket()).isEqualTo(bucket_name);
   }
 
   @Test
   public void testAwsMinimum() throws URISyntaxException {
     // cdms3:/bucket/key
     String cdmS3Uri = schemeCdmS3 + ":" + bucket_name + "?" + query;
+    testUri(cdmS3Uri);
+  }
+
+  @Test
+  public void WithConfigMinimum() throws URISyntaxException {
+    // cdms3:/bucket/key#config
+    String cdmS3Uri = schemeCdmS3 + ":" + bucket_name + "?" + query + "#" + fragment;
     testUri(cdmS3Uri);
   }
 
@@ -60,6 +109,15 @@ public class TestCdmS3Uri {
   }
 
   @Test
+  public void testWithHostAndEndpointPathConfig() throws URISyntaxException {
+    // cdms3://host/path/bucket/key
+    String cdmS3Uri =
+        schemeCdmS3 + "://" + host + "%s/" + endpoint_segments + bucket_name + "?" + query + "#" + fragment;
+    String expectedEndpoint = host + "%s/" + endpoint_segments;
+    gauntlet(cdmS3Uri, expectedEndpoint);
+  }
+
+  @Test
   public void testWithHostAndProfile() throws URISyntaxException {
     // cdms3://profile@host/bucket/key
     String cdmS3Uri = schemeCdmS3 + "://" + user_info + "@" + host + "%s/" + bucket_name + "?" + query;
@@ -68,10 +126,18 @@ public class TestCdmS3Uri {
   }
 
   @Test
+  public void testWithHostProfileCFonfig() throws URISyntaxException {
+    // cdms3://profile@host/bucket/key#config
+    String cdmS3Uri = schemeCdmS3 + "://" + user_info + "@" + host + "%s/" + bucket_name + "?" + query + "#" + fragment;
+    String expectedEndpoint = host + "%s/";
+    gauntlet(cdmS3Uri, expectedEndpoint);
+  }
+
+  @Test
   public void testFull() throws URISyntaxException {
     // cdms3://profile@host/path/bucket/key
-    String cdmS3Uri =
-        schemeCdmS3 + "://" + user_info + "@" + host + "%s/" + endpoint_segments + bucket_name + "?" + query;
+    String cdmS3Uri = schemeCdmS3 + "://" + user_info + "@" + host + "%s/" + endpoint_segments + bucket_name + "?"
+        + query + "#" + fragment;
     String expectedEndpoint = host + "%s/" + endpoint_segments;
     gauntlet(cdmS3Uri, expectedEndpoint);
   }
@@ -120,7 +186,16 @@ public class TestCdmS3Uri {
   }
 
   public void testUri(String cdmS3UriString) throws URISyntaxException {
+    testUriCheckEquals(cdmS3UriString);
     testUri(cdmS3UriString, null);
+  }
+
+  public void testUriCheckEquals(String cdmS3UriString) throws URISyntaxException {
+    CdmS3Uri uri1 = new CdmS3Uri(cdmS3UriString);
+    CdmS3Uri uri2 = new CdmS3Uri(cdmS3UriString);
+    CdmS3Uri uri3 = new CdmS3Uri(cdmS3UriString + "abc");
+    assertThat(uri1).isEqualTo(uri2);
+    assertThat(uri1).isNotEqualTo(uri3);
   }
 
   public void testUri(String cdmS3UriString, String expectedEndpoint) throws URISyntaxException {
@@ -132,6 +207,13 @@ public class TestCdmS3Uri {
     if (expectedEndpoint != null) {
       testEndpoint(cdmS3Uri, URI.create(expectedEndpoint));
     }
+    // now test if key is missing (so only the bucket is defined)
+    int keyStartIndex = cdmS3UriString.indexOf('?');
+    if (keyStartIndex >= 0) {
+      String cdmS3UriStringWithoutKey = cdmS3UriString.substring(0, keyStartIndex);
+      cdmS3Uri = new CdmS3Uri(cdmS3UriStringWithoutKey);
+      testBucket(cdmS3Uri);
+    }
   }
 
   public void testBucket(CdmS3Uri cdmS3Uri) {
@@ -139,7 +221,7 @@ public class TestCdmS3Uri {
   }
 
   public void testKey(CdmS3Uri cdmS3Uri) {
-    assertThat(cdmS3Uri.getKey()).isEqualTo(query);
+    assertThat(cdmS3Uri.getKey().get()).isEqualTo(query);
   }
 
   public void testProfile(CdmS3Uri cdmS3Uri) {
