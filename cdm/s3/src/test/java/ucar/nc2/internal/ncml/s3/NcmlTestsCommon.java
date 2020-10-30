3b666a831bb265eab306a9de216f79fe4ec13dd3
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2020-10-30 04:32:23 -0600

    Add tests for promoteGlobalAttribute, cleanup tests

--- cdm/s3/src/test/java/ucar/nc2/internal/ncml/s3/NcmlTestsCommon.java
+++ cdm/s3/src/test/java/ucar/nc2/internal/ncml/s3/NcmlTestsCommon.java
@@ -9,18 +9,37 @@ import ucar.unidata.io.s3.S3TestsCommon;
 import ucar.unidata.util.test.TestDir;
 
 public class NcmlTestsCommon {
+  ///////////////////////////
   // comparable aggregations
-  static final String joinNewNcmlExplicit = TestDir.localTestDataDir + "ncml/S3JoinNewAggExplicit.ncml";
-  static final String joinNewNcmlScanDelim = TestDir.localTestDataDir + "ncml/S3JoinNewAggScanDelim.ncml";
-  static final String joinNewNcmlScanEnhanced = TestDir.localTestDataDir + "ncml/S3JoinNewAggScanDelimEnhanced.ncml";
-  static final String joinNewNcmlScanNoDelim = TestDir.localTestDataDir + "ncml/S3JoinNewAggScanNoDelim.ncml";
-  static final String joinNewNcmlScanRegExpDelim = TestDir.localTestDataDir + "ncml/S3JoinNewAggScanRegExpDelim.ncml";
+  // explicitly aggregate individual objects
+  static final String joinNewNcmlExplicit = TestDir.localTestDataDir + "ncml/joinNewExplicit/S3JoinNewAggExplicit.ncml";
+  // join new, scan
+  // extract date from object name, filter object path on suffix
+  static final String joinNewNcmlScanSuffixDelim = TestDir.localTestDataDir + "ncml/joinNewScan/suffix/S3Delim.ncml";
+  static final String joinNewNcmlScanSuffixNoDelim =
+      TestDir.localTestDataDir + "ncml/joinNewScan/suffix/S3NoDelim.ncml";
+  // same, but add enhance="all" to netcdf element
+  static final String joinNewNcmlScanEnhanced =
+      TestDir.localTestDataDir + "ncml/joinNewScan/suffix/S3DelimEnhanced.ncml";
+  // join new, scan
+  // extract date from object name, filter object name using regular expression
+  static final String joinNewNcmlScanRegExpDelim = TestDir.localTestDataDir + "ncml/joinNewScan/regExp/S3Delim.ncml";
   static final String joinNewNcmlScanRegExpNoDelim =
-      TestDir.localTestDataDir + "ncml/S3JoinNewAggScanRegExpNoDelim.ncml";
+      TestDir.localTestDataDir + "ncml/joinNewScan/regExp/S3NoDelim.ncml";
+  // join new, scan
+  // promote global attribute in each component file to a variable to create time variable, filter filter object path
+  // on suffix
+  static final String joinNewNcmlScanPromoteDelim = TestDir.localTestDataDir + "ncml/joinNewScan/promote/S3Delim.ncml";
+  static final String joinNewNcmlScanPromoteNoDelim =
+      TestDir.localTestDataDir + "ncml/joinNewScan/promote/S3NoDelim.ncml";
+
   static final int expectedNumberOfTimesInAgg = 12;
   static final int expectedNumberOfTimesInAggRegExp = 2;
   static final String dataVarName = "/Rad";
+  static final String timeDimName = "/time";
   static final String timeVarName = "/time";
+  static final String timeVarNamePromote = "/times";
+  static final String expectedTimeVarName = "/expected_time";
 
   static final String firstObjectLocation = S3TestsCommon.TOP_LEVEL_AWS_BUCKET
       + "?ABI-L1b-RadC/2017/242/00/OR_ABI-L1b-RadC-M3C01_G16_s20172420002168_e20172420004540_c20172420004583.nc";
