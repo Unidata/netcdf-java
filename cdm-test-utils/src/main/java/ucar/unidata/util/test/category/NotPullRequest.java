a0e625d1877d4b6e364a760988b0578ed2f25005
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2021-02-21 06:54:35 -0700

    Replace NotTravis test category with NotPullRequest.

--- cdm-test-utils/src/main/java/ucar/unidata/util/test/category/NotPullRequest.java
+++ cdm-test-utils/src/main/java/ucar/unidata/util/test/category/NotPullRequest.java
@@ -2,10 +2,10 @@ package ucar.unidata.util.test.category;
 
 /**
  * A marker to be used with JUnit categories to indicate that a test method or test class cannot succeed when run
- * within the Travis CI environment.
+ * within the CI environment (currently GitHub Actions) used for checking pull requests.
  *
  * @author cwardgar
  * @since 2015/03/19
  */
-public interface NotTravis {
+public interface NotPullRequest {
 }
