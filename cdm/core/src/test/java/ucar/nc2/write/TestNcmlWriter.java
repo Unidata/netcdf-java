b5ba4a95ca7cc3ceb64b94688c9b6a02ef61eb44
JohnLCaron <jcaron1129@gmail.com>
2021-02-22 18:47:48 -0700

    Fix issue #103, ternary operator result must have the same type.

--- cdm/core/src/test/java/ucar/nc2/write/TestNcmlWriter.java
+++ cdm/core/src/test/java/ucar/nc2/write/TestNcmlWriter.java
@@ -62,4 +62,40 @@ public class TestNcmlWriter {
     String expected = "<values xmlns=\"http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2\">3 25 47</values>\n";
     assertThat(ncml).endsWith(expected);
   }
+
+  @Test
+  public void testMakeValuesElementUInteger() throws IOException {
+    Variable var = Variable.builder().setName("name").setDataType(DataType.UINT).setDimensionsAnonymous(new int[] {3})
+        .setAutoGen(-33, 22).build(makeDummyGroup());
+
+    ucar.array.Array<?> data = var.readArray();
+    System.out.printf("data = %s%n", data);
+
+    NcmlWriter writer = new NcmlWriter();
+    Element elem = writer.makeValuesElement(var, false);
+    String ncml = writer.writeToString(elem);
+
+    System.out.printf("ncml = %s%n", ncml);
+    String expected = "<values xmlns=\"http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2\">-33 -11 11</values>\n";
+    assertThat(ncml).endsWith(expected);
+  }
+
+  @Test
+  public void testMakeValuesElementAllowRegular() throws IOException {
+    Variable var = Variable.builder().setName("name").setDataType(DataType.DOUBLE).setDimensionsAnonymous(new int[] {3})
+        .setAutoGen(3.2, 2).build(makeDummyGroup());
+
+    ucar.array.Array<?> data = var.readArray();
+    System.out.printf("data = %s%n", data);
+
+    NcmlWriter writer = new NcmlWriter();
+    Element elem = writer.makeValuesElement(var, true);
+    String ncml = writer.writeToString(elem);
+
+    System.out.printf("ncml = %s%n", ncml);
+    String expected =
+        "<values xmlns=\"http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2\" start=\"3.2\" increment=\"2.0\" npts=\"3\" />\n";
+    assertThat(ncml).endsWith(expected);
+  }
+
 }
