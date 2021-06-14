4b71690502fe9602f987411c860e6e232cacd430
Isabelle Pfander <irpfander@gmail.com>
2021-06-14 16:40:14 -0700

    Fixed deprecation

--- docs/src/test/java/tests/featuretypes/TestGridDatasetsTutorial.java
+++ docs/src/test/java/tests/featuretypes/TestGridDatasetsTutorial.java
@@ -1,4 +1,5 @@
 package tests.featuretypes;
+
 import examples.featuretypes.GridDatasetsTutorial;
 import org.junit.Assert;
 import org.junit.Test;
@@ -6,54 +7,55 @@ import ucar.ma2.InvalidRangeException;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
 public class TestGridDatasetsTutorial {
 
-    @Test
-    public void testGridDatasetFormat()  {
-        // test open success
-        Assert.assertThrows(FileNotFoundException.class, () -> {
-            GridDatasetsTutorial.gridDatasetFormat(null,  "yourLocationAsString", null);
-        });
-    }
+  @Test
+  public void testGridDatasetFormat() {
+    // test open success
+    Assert.assertThrows(FileNotFoundException.class, () -> {
+      GridDatasetsTutorial.gridDatasetFormat(null, "yourLocationAsString", null);
+    });
+  }
 
-    @Test
-    public void testGridFormat()  {
-        // test open success
-        Assert.assertThrows(FileNotFoundException.class, () -> {
-            GridDatasetsTutorial.gridFormat("yourLocationAsString");
-        });
-    }
+  @Test
+  public void testGridFormat() {
+    // test open success
+    Assert.assertThrows(FileNotFoundException.class, () -> {
+      GridDatasetsTutorial.gridFormat("yourLocationAsString");
+    });
+  }
 
-    @Test
-    public void testUsingGridDataset()  {
-        // test open success
-        Assert.assertThrows(NullPointerException.class, () -> {
-            GridDatasetsTutorial.usingGridDataset("yourLocationAsString",   null);
-        });
-    }
+  @Test
+  public void testUsingGridDataset() {
+    // test open success
+    Assert.assertThrows(NullPointerException.class, () -> {
+      GridDatasetsTutorial.usingGridDataset("yourLocationAsString", null);
+    });
+  }
 
-    @Test
-    public void testFindLatLonVal()  {
-        // test open success
-        Assert.assertThrows(FileNotFoundException.class, () -> {
-            GridDatasetsTutorial.findLatLonVal("yourLocationAsString",   null, 0, 0);
-        });
-    }
+  @Test
+  public void testFindLatLonVal() {
+    // test open success
+    Assert.assertThrows(FileNotFoundException.class, () -> {
+      GridDatasetsTutorial.findLatLonVal("yourLocationAsString", null, 0, 0);
+    });
+  }
 
-    @Test
-    public void testReadingData() throws InvalidRangeException, IOException {
-        // test open success
-        Assert.assertThrows(NullPointerException.class, () -> {
-            GridDatasetsTutorial.readingData(null,  0, 0, 0, 0);
-        });
-    }
+  @Test
+  public void testReadingData() throws InvalidRangeException, IOException {
+    // test open success
+    Assert.assertThrows(NullPointerException.class, () -> {
+      GridDatasetsTutorial.readingData(null, 0, 0, 0, 0);
+    });
+  }
 
-    @Test
-    public void testCallMakeSubset() throws IOException {
-        // test open success
-        Assert.assertThrows(NullPointerException.class, () -> {
-            GridDatasetsTutorial.CallMakeSubset(null, null, null, null, null, null , null);
-        });
-    }
+  @Test
+  public void testCallMakeSubset() throws IOException {
+    // test open success
+    Assert.assertThrows(NullPointerException.class, () -> {
+      GridDatasetsTutorial.CallMakeSubset(null, null, null, null, null, null, null);
+    });
+  }
 
 }
