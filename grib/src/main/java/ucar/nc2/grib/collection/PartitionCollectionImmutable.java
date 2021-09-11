81e29ceada9064b461617ff95a3c2c6a60198ea8
JohnLCaron <jcaron1129@gmail.com>
2021-09-11 09:45:18 -0600

    finish removing coverage from grib

--- grib/src/main/java/ucar/nc2/grib/collection/PartitionCollectionImmutable.java
+++ grib/src/main/java/ucar/nc2/grib/collection/PartitionCollectionImmutable.java
@@ -11,7 +11,6 @@ import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import thredds.featurecollection.FeatureCollectionConfig;
-import ucar.nc2.ft2.coverage.SubsetParams;
 import ucar.nc2.grib.coord.*;
 import ucar.nc2.dataset.DatasetUrl;
 import ucar.nc2.grib.GdsHorizCoordSys;
@@ -703,52 +702,7 @@ public abstract class PartitionCollectionImmutable extends GribCollectionImmutab
         return -1;
       return part.getIndex(val);
     }
-
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // experimental coord based
-    @Nullable
-    DataRecord getDataRecord(SubsetParams coords) throws IOException {
-
-      // identify the master index for this runtime
-      CalendarDate runtime = coords.getRunTime();
-      int masterIdx = masterRuntime.getIndex(runtime.getMillisFromEpoch());
-      // LOOK ok to use Best like this (see other getDataRecord) ?
-
-      if (masterIdx < 0) { // means that the runtie is not in the masterRuntime list
-        throw new RuntimeException("masterRuntime does not contain runtime " + runtime);
-      }
-
-      // each runtime is mapped to a partition
-      int partno = run2part[masterIdx];
-      if (partno < 0)
-        return null; // LOOK is this possible?
-
-      // find the 2D vi in that partition
-      GribCollectionImmutable.VariableIndex compVindex2D = getVindex2D(partno); // the 2D component variable in the
-      // partno partition
-      if (compVindex2D == null)
-        return null; // missing
-      if (Grib.debugRead)
-        logger.debug("  compVindex2D = {}", compVindex2D.toStringFrom());
-
-      if (isPartitionOfPartitions) {
-        VariableIndexPartitioned compVindex2Dp = (VariableIndexPartitioned) compVindex2D;
-        return compVindex2Dp.getDataRecord(coords);
-      }
-
-      // otherwise its a GribCollection
-      GribCollectionImmutable.Record record = compVindex2D.getRecordAt(coords);
-      if (record == null)
-        return null;
-
-      if (Grib.debugRead)
-        logger.debug("  result success: partno={} fileno={}", partno, record.fileno);
-      DataRecord dr =
-          new DataRecord(PartitionCollectionImmutable.this, partno, compVindex2D.group.getGdsHorizCoordSys(), record);
-      return dr;
-    }
   }
-
 
   @Immutable
   static class DataRecord extends GribReaderRecord {
