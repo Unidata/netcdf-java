97403abfbe77f55d34aa0542e5ae52916c4c31e0
JohnLCaron <jcaron1129@gmail.com>
2021-10-09 06:41:40 -0600

    Delete ucar.nc2.time and uk...time

--- cdm-core/src/main/java/uk/ac/rdg/resc/edal/time/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 1998-2021 John Caron and University Corporation for Atmospheric Research/Unidata
- * See LICENSE for license information.
- */
-
-/**
- * DEPRECATED API - THIS AND ALL SUBPACKAGES WILL BE REPLACED: Calender implementations.
- */
-package uk.ac.rdg.resc.edal.time;
