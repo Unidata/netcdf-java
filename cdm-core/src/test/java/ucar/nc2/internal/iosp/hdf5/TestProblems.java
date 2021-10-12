37c8d3e7c27039767aa77d616df7be1ca973da94
JohnLCaron <jcaron1129@gmail.com>
2021-10-12 17:14:10 -0600

    Need @Category(NeedsCdmUnitTest.class)

--- cdm-core/src/test/java/ucar/nc2/internal/iosp/hdf5/TestProblems.java
+++ cdm-core/src/test/java/ucar/nc2/internal/iosp/hdf5/TestProblems.java
@@ -5,16 +5,19 @@
 package ucar.nc2.internal.iosp.hdf5;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import ucar.array.ArrayType;
 import ucar.nc2.EnumTypedef;
 import ucar.nc2.NetcdfFile;
 import ucar.nc2.NetcdfFiles;
 import ucar.nc2.Variable;
 import ucar.unidata.util.test.TestDir;
+import ucar.unidata.util.test.category.NeedsCdmUnitTest;
 
 import static com.google.common.truth.Truth.assertThat;
 
 /** Test problems in hdf5 / netcdf 4 files. */
+@Category(NeedsCdmUnitTest.class)
 public class TestProblems {
 
   @Test
