bcc089b4fd4deb1d3dcaa8acdb0949e6814a351b
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2021-12-24 07:17:30 -0700

    Create fresh gradle build using 7.3.3

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+tasks.wrapper {
+    gradleVersion = "7.3.3"
+    distributionSha256Sum = "c9490e938b221daf0094982288e4038deed954a3f12fb54cbf270ddf4e37d879"
+    distributionType = Wrapper.DistributionType.ALL
+}
