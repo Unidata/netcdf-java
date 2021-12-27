6aaa8370e6e8fe2ecb741d426a2abad478b1944a
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2021-12-27 15:42:55 -0700

    Fix cdm-core test to run on windows

--- cdm-core/src/test/java/ucar/nc2/grid/TestGridDataset.java
+++ cdm-core/src/test/java/ucar/nc2/grid/TestGridDataset.java
@@ -29,8 +29,8 @@ public class TestGridDataset {
     try (GridDataset gds = GridDatasetFactory.openGridDataset(filename, errlog)) {
       assertThat(gds).isNotNull();
       System.out.println("readGridDataset: " + gds.getLocation());
-      assertThat(gds.toString()).startsWith("name = GFS_Puerto_Rico_191km_20090729_0000.nc\n"
-          + "location = ../cdm-core/src/test/data/ncml/fmrc/GFS_Puerto_Rico_191km_20090729_0000.nc\n"
+      assertThat(gds.toString()).startsWith("name = GFS_Puerto_Rico_191km_20090729_0000.nc" + System.lineSeparator()
+          + "location = ../cdm-core/src/test/data/ncml/fmrc/GFS_Puerto_Rico_191km_20090729_0000.nc" + System.lineSeparator()
           + "featureType = GRID");
 
       Grid grid = gds.findGridByAttribute("Grib_Variable_Id", "VAR_7-0-2-11_L100").orElseThrow();
@@ -53,8 +53,11 @@ public class TestGridDataset {
       assertThat((Object) gcs.findCoordAxisByType(AxisType.Time)).isNotNull();
       assertThat((Object) gcs.findCoordAxisByType(AxisType.Ensemble)).isNull();
 
-      assertThat(gcs.toString()).startsWith("Coordinate System (time isobaric1 y x)\n" + " time (GridAxisPoint) \n"
-          + " isobaric1 (GridAxisPoint) \n" + " y (GridAxisPoint) \n" + " x (GridAxisPoint) ");
+      assertThat(gcs.toString()).startsWith("Coordinate System (time isobaric1 y x)" + System.lineSeparator()
+              + " time (GridAxisPoint) " + System.lineSeparator()
+              + " isobaric1 (GridAxisPoint) " + System.lineSeparator()
+              + " y (GridAxisPoint) " + System.lineSeparator()
+              + " x (GridAxisPoint) ");
 
       assertThat(gcs.showFnSummary()).isEqualTo("GRID(T,Z,Y,X)");
     }
