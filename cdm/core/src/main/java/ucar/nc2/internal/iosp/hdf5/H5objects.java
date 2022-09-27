d69c058559f9ad9ad603a5a09b8119dd39562a38
Dennis Heimbigner <dmh@ucar.edu>
2022-09-27 16:30:11 -0600

    run8

--- cdm/core/src/main/java/ucar/nc2/internal/iosp/hdf5/H5objects.java
+++ cdm/core/src/main/java/ucar/nc2/internal/iosp/hdf5/H5objects.java
@@ -1703,7 +1703,7 @@ public class H5objects {
         // create properties map
         Map<String, Object> props = new HashMap<>();
         // read filter description
-        int id = (int)raf.readShort();
+        int id = (int) raf.readShort();
         props.put(Filters.Keys.ID, id);
         // if the filter id < 256 then this field is not stored
         short nameSize = ((version > 1) && (id < 256)) ? 0 : raf.readShort();
@@ -1772,7 +1772,7 @@ public class H5objects {
     }
 
     int getId() {
-      return (int)this.properties.get(Filters.Keys.ID);
+      return (int) this.properties.get(Filters.Keys.ID);
     }
 
     Map<String, Object> getProperties() {
