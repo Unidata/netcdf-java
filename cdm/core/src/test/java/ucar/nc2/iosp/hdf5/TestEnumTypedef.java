b35601b9608900f2dab436a78576385c65ae251e
Dennis Heimbigner <dmh@ucar.edu>
2022-09-29 16:10:53 -0600

    log1

--- cdm/core/src/test/java/ucar/nc2/iosp/hdf5/TestEnumTypedef.java
+++ cdm/core/src/test/java/ucar/nc2/iosp/hdf5/TestEnumTypedef.java
@@ -3,6 +3,8 @@ package ucar.nc2.iosp.hdf5;
 
 import static com.google.common.truth.Truth.assertThat;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ucar.ma2.DataType;
 import ucar.nc2.EnumTypedef;
 import ucar.nc2.NetcdfFile;
@@ -11,11 +13,14 @@ import ucar.nc2.Variable;
 import ucar.unidata.util.test.TestDir;
 
 /** Test handling of enums in hdf5 / netcdf 4 files. */
+// see Issue #126
 public class TestEnumTypedef {
+  private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   @Test
-  public void test() throws Exception {
+  public void problem() throws Exception {
     String s = TestDir.cdmLocalTestDataDir + "hdf5/test_atomic_types.nc";
+    logger.info("TestEnumTypedef on {}%n", filename);
     try (NetcdfFile ncfile = NetcdfFiles.open(s)) {
       Variable primaryCloud = ncfile.findVariable("primary_cloud");
       assertThat((Object) primaryCloud).isNotNull();
@@ -24,7 +29,7 @@ public class TestEnumTypedef {
       assertThat(primaryCloud.getEnumTypedef()).isNotNull();
       EnumTypedef typedef = primaryCloud.getEnumTypedef();
       assertThat(typedef).isNotNull();
-      // see Issue #126
+      logger.info("TestEnumTypedef typedef name {}%n", typedef.getShortName());
       assertThat(typedef.getShortName()).isEqualTo("cloud_class_t");
     }
   }
