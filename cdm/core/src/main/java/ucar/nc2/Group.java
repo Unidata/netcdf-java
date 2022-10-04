0bdfb512f201e82fd9421199b23124d87b50d058
Dennis Heimbigner <dmh@ucar.edu>
2022-10-04 13:50:52 -0600

    Force re-test

--- cdm/core/src/main/java/ucar/nc2/Group.java
+++ cdm/core/src/main/java/ucar/nc2/Group.java
@@ -2,6 +2,7 @@
  * Copyright (c) 1998-2018 University Corporation for Atmospheric Research/Unidata
  * See LICENSE for license information.
  */
+
 package ucar.nc2;
 
 import static java.util.Optional.ofNullable;
