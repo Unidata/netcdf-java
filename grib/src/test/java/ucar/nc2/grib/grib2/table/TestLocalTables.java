fc6cd3db6a4526cf7c0e3b1b831e5bb4b47d21cd
Tara Drwenski <tdrwenski@ucar.edu>
2022-11-02 14:29:49 -0600

    Revert "Add test for the grib2 forecast intervals with a unit converter"
    This reverts commit c8abd5efadb82655a3c5be1ae12f9b68bdb5b2a7.

--- grib/src/test/java/ucar/nc2/grib/grib2/table/TestLocalTables.java
+++ grib/src/test/java/ucar/nc2/grib/grib2/table/TestLocalTables.java
@@ -6,31 +6,14 @@ import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import java.io.IOException;
-import java.lang.invoke.MethodHandles;
 import java.util.Iterator;
-import java.util.List;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import thredds.featurecollection.FeatureCollectionConfig.TimeUnitConverterHash;
-import thredds.inventory.CollectionUpdateType;
-import thredds.inventory.MFile;
-import thredds.inventory.MFiles;
-import ucar.nc2.grib.GribIndex;
 import ucar.nc2.grib.GribTables;
-import ucar.nc2.grib.coord.TimeCoordIntvDateValue;
-import ucar.nc2.grib.grib2.Grib2Index;
-import ucar.nc2.grib.grib2.Grib2Record;
-import ucar.nc2.time.CalendarDate;
-import ucar.unidata.util.test.TestDir;
-import ucar.unidata.util.test.category.NeedsCdmUnitTest;
 
 @RunWith(JUnit4.class)
 public class TestLocalTables {
-  private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   @Test
   public void testLocalTables() throws IOException {
@@ -73,44 +56,5 @@ public class TestLocalTables {
         assertThat(ecmwfTable.getCodeTableValue(tableName, entry.getCode())).isEqualTo(entry.getName());
       }
     }
-  }
-
-  @Test
-  @Category(NeedsCdmUnitTest.class)
-  public void shouldUseUnitConverterWhenCreatingForecastInterval() throws IOException {
-    final MFile file =
-        MFiles.create(TestDir.cdmUnitTestDir + "/tds/ncep/NDFD_NWS_CONUS_conduit_2p5km_20220816_0030.grib2");
-    assertThat(file).isNotNull();
-
-    final Grib2Index gribIndex =
-        (Grib2Index) GribIndex.readOrCreateIndexFromSingleFile(false, file, CollectionUpdateType.always, logger);
-    final List<Grib2Record> records = gribIndex.getRecords();
-    assertThat(records).isNotEmpty();
-    final Grib2Record record = records.get(0);
-    final Grib2Tables tables = Grib2Tables.factory(record);
-
-    final CalendarDate start = CalendarDate.parseISOformat(null, "2022-08-18T00:00:00Z");
-    final CalendarDate end = CalendarDate.parseISOformat(null, "2022-08-18T12:00:00Z");
-    final TimeCoordIntvDateValue expectedInterval = new TimeCoordIntvDateValue(start, end);
-
-    // default units
-    final TimeCoordIntvDateValue interval = tables.getForecastTimeInterval(record);
-    assertThat(interval).isEqualTo(expectedInterval);
-
-    // convert hours to minutes
-    final TimeUnitConverterHash hoursToMinutesConverter = new TimeUnitConverterHash();
-    hoursToMinutesConverter.map.put(1, 0);
-    tables.setTimeUnitConverter(hoursToMinutesConverter);
-
-    final TimeCoordIntvDateValue intervalHoursToMinutes = tables.getForecastTimeInterval(record);
-    assertThat(intervalHoursToMinutes).isEqualTo(expectedInterval);
-
-    // convert minutes to hour
-    final TimeUnitConverterHash minutesToHoursConverter = new TimeUnitConverterHash();
-    minutesToHoursConverter.map.put(0, 1);
-    tables.setTimeUnitConverter(minutesToHoursConverter);
-
-    final TimeCoordIntvDateValue intervalMinutesToHours = tables.getForecastTimeInterval(record);
-    assertThat(intervalMinutesToHours).isEqualTo(expectedInterval);
   }
 }
