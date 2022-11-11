5324d8b8be4a7d9f8f64cc02e1e84698fbe63667
Tara Drwenski <tdrwenski@ucar.edu>
2022-11-11 13:26:42 -0700

    Use undeprecated version of acquireDataset

--- cdm-test/src/test/java/ucar/nc2/ncml/TestOffAggUpdating.java
+++ cdm-test/src/test/java/ucar/nc2/ncml/TestOffAggUpdating.java
@@ -17,7 +17,7 @@ import org.slf4j.LoggerFactory;
 import ucar.nc2.NetcdfFile;
 import ucar.nc2.Variable;
 import ucar.nc2.dataset.DatasetUrl;
-import ucar.nc2.dataset.NetcdfDataset;
+import ucar.nc2.dataset.NetcdfDatasets;
 import ucar.nc2.util.CancelTask;
 import ucar.nc2.util.cache.FileCacheable;
 import ucar.unidata.util.test.category.NeedsCdmUnitTest;
@@ -105,7 +105,7 @@ public class TestOffAggUpdating {
     DatasetUrl durl = DatasetUrl.findDatasetUrl(location);
 
     // open the agg
-    NetcdfFile ncfile = NetcdfDataset.acquireDataset(new NcmlStringFileFactory(), durl, null, -1, null, null);
+    NetcdfFile ncfile = NetcdfDatasets.acquireDataset(new NcmlStringFileFactory(), durl, null, -1, null, null);
 
     check(ncfile, expectedSizeOfTime);
 
