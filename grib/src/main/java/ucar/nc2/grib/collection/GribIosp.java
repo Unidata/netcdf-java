acc774817a3e42199c7ac2978df4e2b7526abef5
Ethan Davis <edavis@ucar.edu>
2023-01-05 13:47:56 -0700

    Handle 2D case for time coords (e.g., more than one model run).

--- grib/src/main/java/ucar/nc2/grib/collection/GribIosp.java
+++ grib/src/main/java/ucar/nc2/grib/collection/GribIosp.java
@@ -786,40 +786,41 @@ public abstract class GribIosp extends AbstractIOServiceProvider {
 
       case intvU:
         // data[nruns*ntimes]
+        int dataIndex = 0;
         double[] currentBounds = new double[2];
         double[] prevBounds = new double[2];
         for (int runIdx = 0; runIdx < nruns; runIdx++) {
           CoordinateTimeIntv timeIntv = (CoordinateTimeIntv) time2D.getTimeCoordinate(runIdx);
+          int runOffsetIndex = runIdx * ntimes;
           int timeUnitValue = timeUnit.getValue();
           int time2D_Offset = time2D.getOffset(runIdx);
-          int timeIndex = 0;
           for (TimeCoordIntvValue tinv : timeIntv.getTimeIntervals()) {
             currentBounds[0] = timeUnitValue * tinv.getBounds1() + time2D_Offset;
             currentBounds[1] = timeUnitValue * tinv.getBounds2() + time2D_Offset;
             // Use end-point of current interval as initial guess for current time coordinate value
-            data[timeIndex] = currentBounds[1];
-            if (timeIndex >= 1) {
+            data[dataIndex] = currentBounds[1];
+            if (dataIndex >= runOffsetIndex + 1) {
               // Check that time coordinate values are increasing in a strictly-monotonic manner
               // (as required by CF conventions). If not strictly-monotonic ...
-              if (data[timeIndex] <= data[timeIndex - 1]) {
-                if (timeIndex >= 2) {
-                  if (data[timeIndex - 2] <= currentBounds[0]) {
+              if (data[dataIndex] <= data[dataIndex - 1]) {
+                if (dataIndex >= runOffsetIndex + 2) {
+                  if (data[dataIndex - 2] <= currentBounds[0]) {
                     // Change previous time coordinate value to mid-point between
                     // current time interval start and end values.
-                    data[timeIndex - 1] = (currentBounds[1] - currentBounds[0]) / 2.0 + currentBounds[0];
+                    data[dataIndex - 1] = (currentBounds[1] - currentBounds[0]) / 2.0 + currentBounds[0];
                   } else {
                     // Or change previous time coordinate value to mid-point between
                     // current time interval end value and the time coord value from two steps back.
-                    data[timeIndex - 1] = (currentBounds[1] - data[timeIndex - 2]) / 2.0 + data[timeIndex - 2];
+                    data[dataIndex - 1] = (currentBounds[1] - data[dataIndex - 2]) / 2.0 + data[dataIndex - 2];
                   }
                 } else {
-                  data[timeIndex - 1] = (prevBounds[1] - prevBounds[0]) / 2.0 + prevBounds[0];
+                  data[dataIndex - 1] = (prevBounds[1] - prevBounds[0]) / 2.0 + prevBounds[0];
                 }
               }
             }
             prevBounds[0] = currentBounds[0];
             prevBounds[1] = currentBounds[1];
-            timeIndex++;
+            dataIndex++;
           }
         }
         // The above assumes that sets of intervals are always sorted by
