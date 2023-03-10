2b8a2c10e44e338ff5b798551b5143d85c268138
Tara Drwenski <tdrwenski@ucar.edu>
2023-03-10 15:24:01 -0700

    Replace anonymous class with lambda

--- cdm/gcdm/src/main/java/ucar/gcdm/server/GcdmServer.java
+++ cdm/gcdm/src/main/java/ucar/gcdm/server/GcdmServer.java
@@ -43,19 +43,16 @@ public class GcdmServer {
     /* The port on which the server should run */
     int port = 16111;
     server = ServerBuilder.forPort(port).addService(new GcdmImpl()).build().start();
-    Runtime.getRuntime().addShutdownHook(new Thread() {
-      @Override
-      public void run() {
-        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-        System.err.println("*** shutting down gRPC server since JVM is shutting down");
-        try {
-          GcdmServer.this.stop();
-        } catch (InterruptedException e) {
-          e.printStackTrace(System.err);
-        }
-        System.err.println("*** server shut down");
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+      System.err.println("*** shutting down gRPC server since JVM is shutting down");
+      try {
+        GcdmServer.this.stop();
+      } catch (InterruptedException e) {
+        e.printStackTrace(System.err);
       }
-    });
+      System.err.println("*** server shut down");
+    }));
 
     logger.info("Server started, listening on " + port);
     System.out.println("---> Server started, listening on " + port); // Used for gradle startDaemon
