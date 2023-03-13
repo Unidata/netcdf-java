50388612c570ca15ea9ff53affa631f99d051e49
Tara Drwenski <tdrwenski@ucar.edu>
2023-03-13 15:20:57 -0600

    Use protocol variable instead of literal

--- cdm/gcdm/src/main/java/ucar/gcdm/client/GcdmNetcdfFileProvider.java
+++ cdm/gcdm/src/main/java/ucar/gcdm/client/GcdmNetcdfFileProvider.java
@@ -9,7 +9,7 @@ import ucar.nc2.util.CancelTask;
 public class GcdmNetcdfFileProvider implements NetcdfFileProvider {
   @Override
   public String getProtocol() {
-    return "gcdm";
+    return GcdmNetcdfFile.PROTOCOL;
   }
 
   @Override
