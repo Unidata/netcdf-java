183a77df9da6045009d95b37cf00f2a27878d3da
Dennis Heimbigner <dmh@ucar.edu>
2023-04-06 12:20:57 -0600

    Update to match latest main

--- cdm/core/src/main/java/ucar/nc2/internal/iosp/hdf5/H5headerNew.java
+++ cdm/core/src/main/java/ucar/nc2/internal/iosp/hdf5/H5headerNew.java
@@ -1649,7 +1649,7 @@ public class H5headerNew implements H5headerIF, HdfHeaderIF {
 
     // chunked stuff
     boolean isChunked;
-    public DataBTree btree; // only if isChunked
+    DataBTree btree; // only if isChunked
 
     MessageDatatype mdt;
     MessageDataspace mds;
