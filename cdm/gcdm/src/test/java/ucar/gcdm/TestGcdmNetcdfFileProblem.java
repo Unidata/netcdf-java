ede53e4fdf15a7cfd400d3e0fa4c0b89a4c7647e
Tara Drwenski <tdrwenski@ucar.edu>
2023-05-10 10:12:54 -0600

    Add TestUtils for comparing gcdm and non-gcdm files

--- cdm/gcdm/src/test/java/ucar/gcdm/TestGcdmNetcdfFileProblem.java
+++ cdm/gcdm/src/test/java/ucar/gcdm/TestGcdmNetcdfFileProblem.java
@@ -5,10 +5,11 @@
 package ucar.gcdm;
 
 import static com.google.common.truth.Truth.assertThat;
+import static ucar.gcdm.TestUtils.compareFiles;
+import static ucar.gcdm.TestUtils.compareVariables;
 
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
-import java.util.Formatter;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
@@ -29,18 +30,13 @@ import ucar.ma2.InvalidRangeException;
 import ucar.ma2.StructureDataW;
 import ucar.ma2.StructureMembers;
 import ucar.ma2.StructureMembers.MemberBuilder;
-import ucar.nc2.NetcdfFile;
 import ucar.nc2.Sequence;
 import ucar.nc2.Structure;
 import ucar.nc2.Variable;
-import ucar.nc2.dataset.NetcdfDatasets;
-import ucar.nc2.util.CompareNetcdf2;
 import ucar.nc2.write.NetcdfFileFormat;
 import ucar.nc2.write.NetcdfFormatWriter;
 import ucar.unidata.util.test.TestDir;
 import ucar.unidata.util.test.category.NeedsCdmUnitTest;
-
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /** Test {@link GcdmNetcdfFile} problems */
@@ -66,24 +62,21 @@ public class TestGcdmNetcdfFileProblem {
   @Category(NeedsCdmUnitTest.class)
   public void testRequestTooBig() throws Exception {
     String localFilename = TestDir.cdmUnitTestDir + "formats/hdf4/MOD021KM.A2004328.1735.004.2004329164007.hdf";
-    Path path = Paths.get(localFilename);
-    compareArrayToArray(path, "MODIS_SWATH_Type_L1B/Data_Fields/EV_1KM_RefSB");
+    compareVariables(localFilename, "MODIS_SWATH_Type_L1B/Data_Fields/EV_1KM_RefSB");
   }
 
   @Test
   @Category(NeedsCdmUnitTest.class)
   public void testRequestNotTooBig() throws Exception {
     String localFilename = TestDir.cdmUnitTestDir + "formats/hdf4/MOD021KM.A2004328.1735.004.2004329164007.hdf";
-    Path path = Paths.get(localFilename);
-    compareArrayToArray(path, "MODIS_SWATH_Type_L1B/Data_Fields/EV_500_Aggr1km_RefSB_Samples_Used");
+    compareVariables(localFilename, "MODIS_SWATH_Type_L1B/Data_Fields/EV_500_Aggr1km_RefSB_Samples_Used");
   }
 
   @Test
   @Category(NeedsCdmUnitTest.class)
   public void testHdf4() throws Exception {
     String localFilename = TestDir.cdmUnitTestDir + "formats/hdf4/MOD021KM.A2004328.1735.004.2004329164007.hdf";
-    Path path = Paths.get(localFilename);
-    compareArrayToArray(path, "MODIS_SWATH_Type_L1B/Data_Fields/EV_250_Aggr1km_RefSB");
+    compareVariables(localFilename, "MODIS_SWATH_Type_L1B/Data_Fields/EV_250_Aggr1km_RefSB");
   }
 
   @Ignore("This variable is too large and causes an out of memory exception unrelated to gcdm.")
@@ -91,16 +84,14 @@ public class TestGcdmNetcdfFileProblem {
   @Category(NeedsCdmUnitTest.class)
   public void testCombineStructure() throws Exception {
     String localFilename = TestDir.cdmUnitTestDir + "formats/hdf5/IASI/IASI.h5";
-    Path path = Paths.get(localFilename);
-    compareArrayToArray(path, "U-MARF/EPS/IASI_xxx_1C/DATA/MDR_1C_IASI_L1_ARRAY_000001");
+    compareVariables(localFilename, "U-MARF/EPS/IASI_xxx_1C/DATA/MDR_1C_IASI_L1_ARRAY_000001");
   }
 
   @Test
   @Category(NeedsCdmUnitTest.class)
   public void testCombineStructureMember() throws Exception {
     String localFilename = TestDir.cdmUnitTestDir + "formats/hdf5/IASI/IASI.h5";
-    Path path = Paths.get(localFilename);
-    compareArrayToArray(path, "U-MARF/EPS/IASI_xxx_1C/DATA/MDR_1C_IASI_L1_ARRAY_000001.GCcsRadAnalStd-value");
+    compareVariables(localFilename, "U-MARF/EPS/IASI_xxx_1C/DATA/MDR_1C_IASI_L1_ARRAY_000001.GCcsRadAnalStd-value");
   }
 
   @Test
@@ -157,8 +148,7 @@ public class TestGcdmNetcdfFileProblem {
   @Category(NeedsCdmUnitTest.class)
   public void testGcdmVlenCast() throws Exception {
     String localFilename = TestDir.cdmUnitTestDir + "formats/netcdf4/files/tst_opaque_data.nc4";
-    Path path = Paths.get(localFilename);
-    compareArrayToArray(path);
+    compareFiles(localFilename);
   }
 
   @Test
@@ -166,62 +156,54 @@ public class TestGcdmNetcdfFileProblem {
   public void testGcdmProblemNeeds() throws Exception {
     String localFilename =
         TestDir.cdmUnitTestDir + "formats/netcdf4/e562p1_fp.inst3_3d_asm_Nv.20100907_00z+20100909_1200z.nc4";
-    Path path = Paths.get(localFilename);
-    compareArrayToArray(path, "O3");
+    compareVariables(localFilename, "O3");
   }
 
   @Test
   @Category(NeedsCdmUnitTest.class)
   public void testDataTooLarge() throws Exception {
     String localFilename = TestDir.cdmUnitTestDir + "formats/netcdf4/UpperDeschutes_t4p10_swemelt.nc";
-    Path path = Paths.get(localFilename);
-    compareArrayToArray(path);
+    compareFiles(localFilename);
   }
 
   @Test
   @Category(NeedsCdmUnitTest.class)
   public void testAttributeStruct() throws Exception {
     String localFilename = TestDir.cdmUnitTestDir + "formats/netcdf4/attributeStruct.nc";
-    Path path = Paths.get(localFilename);
-    compareArrayToArray(path, "observations");
+    compareVariables(localFilename, "observations");
   }
 
   @Test
   @Category(NeedsCdmUnitTest.class)
   public void testEnumProblem() throws Exception {
     String localFilename = TestDir.cdmUnitTestDir + "formats/netcdf4/tst/tst_enums.nc";
-    Path path = Paths.get(localFilename);
-    compareArrayToArray(path);
+    compareFiles(localFilename);
   }
 
   @Test
   @Category(NeedsCdmUnitTest.class)
   public void testCharProblem() throws Exception {
     String localFilename = TestDir.cdmUnitTestDir + "formats/bufr/userExamples/test1.bufr";
-    Path path = Paths.get(localFilename);
-    compareArrayToArray(path);
+    compareFiles(localFilename);
   }
 
   @Test
   @Category(NeedsCdmUnitTest.class)
   public void testChunkProblem() throws Exception {
     String localFilename = TestDir.cdmUnitTestDir + "formats/netcdf4/multiDimscale.nc4";
-    Path path = Paths.get(localFilename);
-    compareArrayToArray(path);
+    compareFiles(localFilename);
   }
 
   @Test
   public void testOpaqueDataType() throws Exception {
     String localFilename = TestDir.cdmLocalTestDataDir + "hdf5/test_atomic_types.nc";
-    Path path = Paths.get(localFilename);
-    compareArrayToArray(path);
+    compareFiles(localFilename);
   }
 
   @Test
   public void testGcdmProblem2() throws Exception {
     String localFilename = TestDir.cdmLocalTestDataDir + "dataset/SimpleGeos/hru_soil_moist_vlen_3hru_5timestep.nc";
-    Path path = Paths.get(localFilename);
-    compareArrayToArray(path);
+    compareFiles(localFilename);
   }
 
   @Test
@@ -295,34 +277,6 @@ public class TestGcdmNetcdfFileProblem {
 
       ArrayChar data = (ArrayChar) variable.read();
       assertThat(data.getString(0)).isEqualTo("WOLLOGORANG        ");
-    }
-  }
-
-  private static void compareArrayToArray(Path path) throws Exception {
-    String gcdmUrl = gcdmPrefix + path.toAbsolutePath();
-    try (NetcdfFile ncfile = NetcdfDatasets.openFile(path.toString(), null);
-        GcdmNetcdfFile gcdmFile = GcdmNetcdfFile.builder().setRemoteURI(gcdmUrl).build()) {
-
-      Formatter formatter = new Formatter();
-      boolean ok = CompareNetcdf2.compareFiles(ncfile, gcdmFile, formatter, true, false, true);
-      logger.debug(formatter.toString());
-      assertThat(ok).isTrue();
-    }
-  }
-
-  private static void compareArrayToArray(Path path, String varName) throws Exception {
-    String gcdmUrl = gcdmPrefix + path.toAbsolutePath();
-    try (NetcdfFile ncfile = NetcdfDatasets.openFile(path.toString(), null);
-        GcdmNetcdfFile gcdmFile = GcdmNetcdfFile.builder().setRemoteURI(gcdmUrl).build()) {
-
-      Variable ncVar = ncfile.findVariable(varName);
-      Variable gcdmVar = gcdmFile.findVariable(varName);
-
-      Formatter formatter = new Formatter();
-      CompareNetcdf2 compareNetcdf2 = new CompareNetcdf2(formatter, false, true, true);
-      boolean ok = compareNetcdf2.compareVariable(ncVar, gcdmVar, CompareNetcdf2.IDENTITY_FILTER);
-      logger.debug(formatter.toString());
-      assertThat(ok).isTrue();
     }
   }
 
