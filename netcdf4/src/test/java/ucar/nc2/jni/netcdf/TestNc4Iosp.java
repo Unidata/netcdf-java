e5dd4f6ee2de71782a158c8266f75696ddb57755
jessica <css.jessica14@gmail.com>
2023-05-31 16:29:16 -0500

    Cleanup and check for Clibrary

--- netcdf4/src/test/java/ucar/nc2/jni/netcdf/TestNc4Iosp.java
+++ netcdf4/src/test/java/ucar/nc2/jni/netcdf/TestNc4Iosp.java
@@ -1,10 +1,19 @@
 package ucar.nc2.jni.netcdf;
 
+import org.junit.Assume;
+import org.junit.Before;
 import org.junit.Test;
 import ucar.nc2.Attribute;
 import java.io.IOException;
+import ucar.nc2.ffi.netcdf.NetcdfClibrary;
 
-public class TestNc4IospSpec {
+public class TestNc4Iosp {
+
+  @Before
+  public void checkLibrary() {
+    Assume.assumeTrue("Netcdf-4 C library not present", NetcdfClibrary.isLibraryPresent());
+  }
+
   @Test
   public void flushInDefineMode() throws IOException {
     Nc4Iosp nc4Iosp = new Nc4Iosp();
@@ -15,6 +24,5 @@ public class TestNc4IospSpec {
   public void updateAttribute() throws IOException {
     Nc4Iosp nc4Iosp = new Nc4Iosp();
     nc4Iosp.updateAttribute(null, new Attribute("foo", "bar"));
-
   }
 }
