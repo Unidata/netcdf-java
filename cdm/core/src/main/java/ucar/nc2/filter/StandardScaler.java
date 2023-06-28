5108fb69f0f2fbd5a8f5381119c0b1af2e986aa7
jessica <css.jessica14@gmail.com>
2023-06-28 14:08:42 -0500

    changes to array type

--- cdm/core/src/main/java/ucar/nc2/filter/StandardScaler.java
+++ cdm/core/src/main/java/ucar/nc2/filter/StandardScaler.java
@@ -2,8 +2,9 @@ package ucar.nc2.filter;
 
 import java.util.HashMap;
 import java.util.Map;
-import org.apache.commons.math.stat.descriptive.moment.StandardDeviation;
 import org.apache.commons.math.stat.descriptive.SummaryStatistics;
+import ucar.ma2.Array;
+import ucar.ma2.IndexIterator;
 
 public class StandardScaler extends Filter {
 
@@ -13,7 +14,7 @@ public class StandardScaler extends Filter {
   private final double mean;
   private final double stdDev;
 
-  public StandardScaler(double[] arr){
+  public StandardScaler(Array arr){
     mean = calculateMean(arr);
     stdDev = calculateStandardDeviation(arr);
     Map<String, Object> props = new HashMap<>();
@@ -23,36 +24,24 @@ public class StandardScaler extends Filter {
     scaleOffset = new ScaleOffset(props);
   }
 
-//  public double[] flattenArray(double[][] arr) {
-//    double[] result= new double[arr.length * arr[0].length];
-//    int index = 0;
-//    for (int i = 0; i < arr.length; i++) {
-//      for (int j = 0; j < arr[0].length; j++) {
-//        result[index] = arr[i][j];
-//        index++;
-//      }
-//    }
-//    return result;
-//  }
-  private double calculateMean(double[] data) {
-    SummaryStatistics curr = new SummaryStatistics();
-    for (int a = 0; a < data.length; a++) {
-      curr.addValue(data[a]);
+  private double calculateMean(Array arr) {
+    SummaryStatistics cur = new SummaryStatistics();
+    IndexIterator iterArr = arr.getIndexIterator();
+    while (iterArr.hasNext()) {
+      Number value = (Number) iterArr.getObjectNext();
+      cur.addValue(value.doubleValue());
     }
-    return curr.getMean();
+    return cur.getMean();
   }
 
-  private double calculateStandardDeviation(double[] data) {
-    StandardDeviation stdDev = new StandardDeviation();
-    return stdDev.evaluate(data);
-  }
-
-  public double[] calculateStandardScaler(double[] data, double mean, double stdDev){
-    double[] scaledData = new double[data.length];
-    for (int i = 0; i < data.length; i++) {
-      scaledData[i] = (data[i] - mean) / stdDev;
+  private double calculateStandardDeviation(Array arr) {
+    SummaryStatistics cur = new SummaryStatistics();
+    IndexIterator iterArr = arr.getIndexIterator();
+    while (iterArr.hasNext()) {
+      Number value = (Number) iterArr.getObjectNext();
+      cur.addValue(value.doubleValue());
     }
-    return scaledData;
+    return cur.getStandardDeviation();
   }
 
   public double getMean(){
--- cdm/core/src/test/java/ucar/nc2/filter/TestStandardScaler.java
+++ cdm/core/src/test/java/ucar/nc2/filter/TestStandardScaler.java
@@ -1,108 +1,130 @@
 package ucar.nc2.filter;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertEquals;
+import static ucar.ma2.MAMath.nearlyEquals;
 
-import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.util.Arrays;
-import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
+import java.util.Random;
 import org.junit.Test;
 import ucar.ma2.Array;
 import ucar.ma2.DataType;
+import ucar.ma2.IndexIterator;
 
 public class TestStandardScaler {
 
+  public static final double DATA_MEAN = 100.0;
+  public static final double DATA_STDDEV = 10.0;
+  public static final int DATA_LEN = 10;
+  public static double TOLERANCE = 1.0E-10;
+  public static double GAP = 1.0E-2;
+
+  public static double[] createRandData(){
+    double[] randData= new double[DATA_LEN];
+    Random r = new Random();
+    for (int i = 0; i < randData.length; i++) {
+      randData[i] = r.nextGaussian() * DATA_STDDEV + DATA_MEAN;
+    }
+    return randData;
+  }
+
+  double[] randData = createRandData();
+
+  public double[] normalize(double[] sample) {
+    double[] standardizedSample = new double[sample.length];
+    for (int i = 0; i < sample.length; i++) {
+      standardizedSample[i] = (sample[i] - DATA_MEAN) / DATA_STDDEV;
+    }
+    return standardizedSample;
+  }
+
+  public double[] denormalize(double[] sample){
+    double[] descaledData = new double[sample.length];
+    for (int i = 0; i < sample.length; i++) {
+      descaledData[i] = sample[i] * DATA_STDDEV + DATA_MEAN;
+    }
+    return descaledData;
+  }
+
+  public double[] createDoubleArray(Array arr) {
+    double[] out = new double[arr.getShape()[0]];
+    IndexIterator iterArr = arr.getIndexIterator();
+    int index = 0;
+    while (iterArr.hasNext()) {
+      Number value = (Number) iterArr.getObjectNext();
+      out[index] = value.doubleValue();
+    }
+    return out;
+  }
+
+  public Array createArray(double[] arr) {
+    DataType outType = DataType.DOUBLE;
+    Array out = Array.factory(outType, new int[]{arr.length});
+    IndexIterator iterOut = out.getIndexIterator();
+    for (int i = 0; i < arr.length; i++) {
+      iterOut.setObjectNext(arr[i]);
+    }
+    return out;
+  }
+
   @Test
   public void testEncodeDecode() {
-    double[] data = {1.0, 2.0, 3.0, 4.0, 5.0};
-    StandardScaler filter = new StandardScaler(data);
-    byte[] array = new byte[10];
-    byte[] encoded = filter.encode(array);
+    StandardScaler filter = new StandardScaler(createArray(randData));
+    byte[] barray = FilterHelpers.arrayToBytes(Array.makeFromJavaArray(randData), DataType.DOUBLE, ByteOrder.BIG_ENDIAN);
+    byte[] encoded = filter.encode(barray);
     byte[] decoded = filter.decode(encoded);
-    assertThat(decoded).isEqualTo(array);
+    Array convertedDecoded = FilterHelpers.bytesToArray(decoded, DataType.DOUBLE, ByteOrder.BIG_ENDIAN);
+    assertThat(convertedDecoded.getStorage()).isEqualTo(randData);
   }
 
   @Test
   public void testEncode() {
-    double[] data = {1.0, 2.0, 3.0, 4.0, 5.0};
-    StandardScaler filter = new StandardScaler(data);
-    double dataMean = filter.getMean();
-    double dataStdDev = filter.getStdDev();
-    double[] dataStandardScaler = filter.calculateStandardScaler(data, dataMean, dataStdDev);
-//     FilterHelpers helper = new FilterHelpers();
-    byte[] barray = FilterHelpers.arrayToBytes(Array.makeFromJavaArray(data), DataType.DOUBLE, ByteOrder.BIG_ENDIAN);
-//    byte[] barray = createByteArray(data);
+    StandardScaler filter = new StandardScaler(createArray(randData));
+    double[] dataStandardScaler = normalize(randData);
+    byte[] barray = FilterHelpers.arrayToBytes(Array.makeFromJavaArray(randData), DataType.DOUBLE, ByteOrder.BIG_ENDIAN);
     byte[] encoded = filter.encode(barray);
     Array dencoded = FilterHelpers.bytesToArray(encoded, DataType.DOUBLE, ByteOrder.BIG_ENDIAN);
-//    double[] dencoded = createDoubleArray(encoded);
-    assertThat(dencoded.getStorage()).isEqualTo(dataStandardScaler);
+//    assertThat(dencoded.getStorage()).isEqualTo(dataStandardScaler);
+    assertThat(nearlyEquals(dencoded, createArray(dataStandardScaler))).isTrue();
+
   }
-//  @Test
-//  public void testFlattenArray(){
-//    double[][] data2D = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}};
-//    double[] data1D = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
-//    StandardScaler filter = new StandardScaler();
-//    double[] flatData = filter.flattenArray(data2D);
-//    assertThat(flatData).isEqualTo(data1D);
-//  }
+
+  @Test
+  public void testDecode() {
+    StandardScaler filter = new StandardScaler(createArray(randData));
+    double DataMean = filter.getMean();
+    double DataStdDev = filter.getStdDev();
+    double[] dataStandardScaler = denormalize(randData);
+    byte[] barray = FilterHelpers.arrayToBytes(Array.makeFromJavaArray(randData), DataType.DOUBLE, ByteOrder.BIG_ENDIAN);
+    byte[] decoded = filter.decode(barray);
+    Array convertedDecoded = FilterHelpers.bytesToArray(decoded, DataType.DOUBLE, ByteOrder.BIG_ENDIAN);
+    assertThat(convertedDecoded.getStorage()).isEqualTo(dataStandardScaler);
+  }
 
   @Test
   public void testCalculateMean(){
-    double[] data = {1.0, 2.0, 3.0, 4.0, 5.0};
-    StandardScaler filter = new StandardScaler(data);
+//    double[] data = {1.0, 2.0, 3.0, 4.0, 5.0};
+    StandardScaler filter = new StandardScaler(createArray(randData));
     double calcMean = filter.getMean();
-    assertThat(calcMean).isEqualTo(3);
+    assertThat(calcMean).isWithin(GAP).of(DATA_MEAN);
   }
 
   @Test
   public void testCalculateStandardDeviation(){
-    double[] data = {1.0, 2.0, 3.0, 4.0, 5.0};
-    StandardScaler filter = new StandardScaler(data);
+//    double[] data = {1.0, 2.0, 3.0, 4.0, 5.0};
+    StandardScaler filter = new StandardScaler(createArray(randData));
     double calcStd = filter.getStdDev();
-    assertThat(calcStd).isEqualTo(Math.sqrt(2.5));
+    assertThat(calcStd).isWithin(GAP).of(DATA_STDDEV);
   }
 
-  @Test
-  public void testCalculateStandardScaler(){
-    double[] data = {1.0, 2.0, 3.0, 4.0, 5.0};
-    StandardScaler filter = new StandardScaler(data);
-    double dataMean = filter.getMean();
-    double dataStdDev = filter.getStdDev();
-    double[] dataStandardScaler = filter.calculateStandardScaler(data, dataMean, dataStdDev);
-    double[] scaledData = normalize(data);
-    assertThat(dataStandardScaler).isEqualTo(scaledData);
-  }
-
-  public byte[] createByteArray(double[] arr){
-    ByteBuffer bb = ByteBuffer.allocate(arr.length * 8);
-    for (double d: arr) {
-      bb.putDouble(d);
-    }
-    return bb.array();
-  }
-
-  public double[] createDoubleArray(byte[] arr){
-    ByteBuffer bb = ByteBuffer.wrap(arr);
-    double[] doubles = new double[arr.length / 8];
-    for (int i = 0; i < doubles.length; i++) {
-      doubles[i] = bb.getDouble();
-    }
-    return doubles;
-  }
-
-  public double[] normalize(double[] sample) {
-    DescriptiveStatistics stats = new DescriptiveStatistics();
-    for (int i = 0; i < sample.length; i++) {
-      stats.addValue(sample[i]);
-    }
-    double mean = stats.getMean();
-    double standardDeviation = stats.getStandardDeviation();
-    double[] standardizedSample = new double[sample.length];
-    for (int i = 0; i < sample.length; i++) {
-      standardizedSample[i] = (sample[i] - mean) / standardDeviation;
-    }
-    return standardizedSample;
-  }
+//  @Test
+//  public void testNormalize(){
+////    double[] data = {1.0, 2.0, 3.0, 4.0, 5.0};
+//    StandardScaler filter = new StandardScaler(randData);
+//    double dataMean = filter.getMean();
+//    double dataStdDev = filter.getStdDev();
+//    double[] dataStandardScaler = filter.normalize(randData, dataMean, dataStdDev);
+//    double[] scaledData = normalize(randData);
+//    assertThat(dataStandardScaler).usingTolerance(GAP).equals(scaledData);
+//  }
 
 }
