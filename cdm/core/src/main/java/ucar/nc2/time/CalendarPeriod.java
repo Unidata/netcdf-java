824a4759732337f5529070d9e49d55f8282e2118
Tara Drwenski <tdrwenski@ucar.edu>
2023-09-27 12:42:08 -0600

    Round to the nearest int when finding difference in a certain CalendarPeriod

--- cdm/core/src/main/java/ucar/nc2/time/CalendarPeriod.java
+++ cdm/core/src/main/java/ucar/nc2/time/CalendarPeriod.java
@@ -167,7 +167,7 @@ public class CalendarPeriod {
 
   /**
    * Subtract two dates, return difference in units of this period.
-   * If not even, will round down (take the floor)
+   * If not even, will round to nearest int
    * 
    * @param start start date
    * @param end end date
@@ -176,7 +176,8 @@ public class CalendarPeriod {
   public int subtract(CalendarDate start, CalendarDate end) {
     long diff = end.getDifferenceInMsecs(start);
     int thislen = millisecs();
-    return (int) Math.floor(diff / (float) thislen);
+    int signOfDiff = (int) (diff / Math.abs(diff));
+    return signOfDiff * Math.round(Math.abs(diff) / (float) thislen);
   }
 
   /**
@@ -234,7 +235,7 @@ public class CalendarPeriod {
 
   // offset from start to end, in these units
   // start + offset = end
-  // takes the floor when rounding
+  // rounds to nearest int
   public int getOffset(CalendarDate start, CalendarDate end) {
     if (start.equals(end)) {
       return 0;
