0d0831a81188250e428eb879ec0662f9bffbd895
Dennis Heimbigner <dmh@ucar.edu>
2023-11-06 13:16:34 -0700

    Convert List.size()==0 to List.isEmpty()

--- dap4/src/main/java/dap4/core/ce/CEConstraint.java
+++ dap4/src/main/java/dap4/core/ce/CEConstraint.java
@@ -334,7 +334,7 @@ public class CEConstraint {
     Segment seg = findSegment(var);
     if (seg != null)
       slices = seg.slices;
-    if (slices == null || slices.size() == 0)
+    if (slices == null || slices.isEmpty() == 0)
       slices = Universal.universalSlices(var);
     return slices;
   }
