c34b4d64d869e50d4f1d083dcca2f0757b0b515a
Tara Drwenski <tdrwenski@ucar.edu>
2024-04-10 10:38:58 -0500

    Do not match on host or port when comparing test results

--- opendap/src/test/java/opendap/test/TestMisc.java
+++ opendap/src/test/java/opendap/test/TestMisc.java
@@ -116,8 +116,8 @@ public class TestMisc extends UnitTestCommon {
   }
 
   boolean diff(Testcase testcase, String captured) throws Exception {
-    // don't match on port
-    captured = captured.replaceAll("localhost:\\d+", "localhost:8080");
+    // don't match on host and port
+    captured = captured.replaceAll("dods://.*:\\d+/", "dods://localhost:8080/");
 
     // See if the cdl is in a file or a string.
     if (System.getProperty("nodiff") != null)
