d4a163af6f7e7cf8f83fea3b1c788adc6c7a5241
Leo <matakleo@gmail.com>
2024-06-04 16:13:09 -0600

    rehaul

--- cdm/core/src/main/java/ucar/nc2/filter/Classifier.java
+++ cdm/core/src/main/java/ucar/nc2/filter/Classifier.java
@@ -3,9 +3,14 @@ package ucar.nc2.filter;
 import java.io.IOException;
 import ucar.ma2.Array;
 import ucar.ma2.DataType;
+import ucar.ma2.IndexIterator;
 import ucar.nc2.dataset.VariableDS;
 
 public class Classifier implements Enhancement {
+  private Classifier classifier = null;
+  private static Classifier emptyClassifier;
+  private int classifiedVal;
+  private int[] classifiedArray;
 
   public static Classifier createFromVariable(VariableDS var) {
     try {
@@ -13,26 +18,90 @@ public class Classifier implements Enhancement {
       DataType type = var.getDataType();
       return createFromArray(arr, type);
     } catch (IOException e) {
-      return new Classifier();
+      return emptyClassifier();
     }
   }
 
   public static Classifier createFromArray(Array arr, DataType type) {
-    return new Classifier();
+    /** what is the type of array? */
+    return new Classifier(arr, type);
+
   }
 
+  public static Classifier emptyClassifier() {
+    emptyClassifier = new Classifier();
+    return emptyClassifier;
+  }
 
+  /** wEmpty in case data not good */
+  public Classifier() {}
 
-  /** Custom exception for invalid values */
-  public static class InvalidValueException extends Exception {
-    public InvalidValueException(String message) {
-      super(message);
+  private Classifier(Array arr, DataType type) {
+    classifier = new Classifier();
+    /** how about doubles? */
+
+  }
+
+  /** Classify integer array */
+  public int[] classifyIntArray(Array arr) {
+    classifiedArray = new int[(int) arr.getSize()];
+    int i = 0;
+    IndexIterator iterArr = arr.getIndexIterator();
+    while (iterArr.hasNext()) {
+      Number value = (Number) iterArr.getObjectNext();
+      if (value.intValue() < 0) {
+        classifiedArray[i] = 0;
+      } else {
+        classifiedArray[i] = 1;
+      }
+      i++;
     }
+
+    return classifiedArray;
   }
+
+  /** Classify double array */
+  public int[] classifyDoubleArray(Array arr) {
+    int[] classifiedArray = new int[(int) arr.getSize()];
+    int i = 0;
+    IndexIterator iterArr = arr.getIndexIterator();
+    while (iterArr.hasNext()) {
+      Number value = (Number) iterArr.getObjectNext();
+      if (!Double.isNaN(value.doubleValue())) {
+        if (value.doubleValue() < 0) {
+          classifiedArray[i] = 0;
+        } else {
+          classifiedArray[i] = 1;
+        }
+      }
+      i++;
+    }
+    return classifiedArray;
+  }
+
+  /** Classify float array */
+  public int[] classifyFloatArray(Array arr) {
+    int[] classifiedArray = new int[(int) arr.getSize()];
+    int i = 0;
+    IndexIterator iterArr = arr.getIndexIterator();
+    while (iterArr.hasNext()) {
+      Number value = (Number) iterArr.getObjectNext();
+      if (!Float.isNaN(value.floatValue())) {
+        if (value.floatValue() < 0) {
+          classifiedArray[i] = 0;
+        } else {
+          classifiedArray[i] = 1;
+        }
+      }
+      i++;
+    }
+    return classifiedArray;
+  }
+
+
 
   /** for a single double */
   public int classifyArray(double val) {
-    int classifiedVal;
     if (val >= 0) {
       classifiedVal = 1;
     } else {
@@ -44,7 +113,6 @@ public class Classifier implements Enhancement {
 
   /** for a single float */
   public int classifyArray(float val) {
-    int classifiedVal;
     if (val >= 0) {
       classifiedVal = 1;
     } else {
@@ -56,7 +124,6 @@ public class Classifier implements Enhancement {
 
   /** for a single int ? */
   public int classifyArray(int val) {
-    int classifiedVal;
     if (val >= 0) {
       classifiedVal = 1;
     } else {
@@ -71,67 +138,19 @@ public class Classifier implements Enhancement {
    * maybe not needed if enhancement applied only to doubles and floats?
    */
 
-  /** Classify integer array */
-  public int[] classifyArray(int[] inputArray) {
-    int[] classifiedArray = new int[inputArray.length];
 
-    for (int i = 0; i < inputArray.length; i++) {
-      if (inputArray[i] >= 0) {
-        classifiedArray[i] = 1;
-      } else {
-        classifiedArray[i] = 0;
-      }
-    }
-    return classifiedArray;
-  }
-
-  /** Classify double array */
-  public int[] classifyArray(double[] inputArray) throws InvalidValueException {
-    int[] classifiedArray = new int[inputArray.length];
-
-    for (int i = 0; i < inputArray.length; i++) {
-      if (Double.isNaN(inputArray[i])) {
-        throw new InvalidValueException("Array contains NaN values");
-      }
-      if (inputArray[i] >= 0) {
-        classifiedArray[i] = 1;
-      } else {
-        classifiedArray[i] = 0;
-      }
-    }
-
-    return classifiedArray;
-  }
-
-  /** Classify float array */
-  public int[] classifyArray(float[] inputArray) throws InvalidValueException {
-    int[] classifiedArray = new int[inputArray.length];
-
-    for (int i = 0; i < inputArray.length; i++) {
-      if (Float.isNaN(inputArray[i])) {
-        throw new InvalidValueException("Array contains NaN values");
-      }
-      if (inputArray[i] >= 0) {
-        classifiedArray[i] = 1;
-      } else {
-        classifiedArray[i] = 0;
-      }
-    }
-
-    return classifiedArray;
-  }
 
   @Override
   public double convert(double val) {
-    return classifyArray(val);
+    return classifier.classifyArray(val);
   }
 
   public double convert(float val) {
-    return classifyArray(val);
+    return classifier.classifyArray(val);
   }
 
   public double convert(int val) {
-    return classifyArray(val);
+    return classifier.classifyArray(val);
   }
 }
 
--- cdm/core/src/test/java/ucar/nc2/filter/TestClassifier.java
+++ cdm/core/src/test/java/ucar/nc2/filter/TestClassifier.java
@@ -2,8 +2,8 @@ package ucar.nc2.filter;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import ucar.ma2.Array;
 
-import ucar.nc2.filter.Classifier.InvalidValueException;
 
 public class TestClassifier {
   @Test
@@ -11,7 +11,8 @@ public class TestClassifier {
     Classifier classifier = new Classifier();
     int[] input = {1, 2, 3};
     int[] expected = {1, 1, 1};
-    assertArrayEquals(expected, classifier.classifyArray(input));
+    Array DATA = Array.makeFromJavaArray(input);
+    assertArrayEquals(expected, classifier.classifyIntArray(DATA));
   }
 
   @Test
@@ -19,7 +20,8 @@ public class TestClassifier {
     Classifier classifier = new Classifier();
     int[] input = {-1, -2, -3};
     int[] expected = {0, 0, 0};
-    assertArrayEquals(expected, classifier.classifyArray(input));
+    Array DATA = Array.makeFromJavaArray(input);
+    assertArrayEquals(expected, classifier.classifyIntArray(DATA));
   }
 
   @Test
@@ -27,7 +29,8 @@ public class TestClassifier {
     Classifier classifier = new Classifier();
     int[] input = {-1, 2, -3, 4};
     int[] expected = {0, 1, 0, 1};
-    assertArrayEquals(expected, classifier.classifyArray(input));
+    Array DATA = Array.makeFromJavaArray(input);
+    assertArrayEquals(expected, classifier.classifyIntArray(DATA));
   }
 
   @Test
@@ -35,88 +38,86 @@ public class TestClassifier {
     Classifier classifier = new Classifier();
     int[] input = {0, -1, 1, 0, 0, 0};
     int[] expected = {1, 0, 1, 1, 1, 1};
-    assertArrayEquals(expected, classifier.classifyArray(input));
+    Array DATA = Array.makeFromJavaArray(input);
+    assertArrayEquals(expected, classifier.classifyIntArray(DATA));
   }
 
   /** test doubles */
   @Test
-  public void testClassifyDoubleArray_AllPositive() throws InvalidValueException {
+  public void testClassifyDoubleArray_AllPositive() {
     Classifier classifier = new Classifier();
     double[] input = {1.1, 2.2, 3.3};
     int[] expected = {1, 1, 1};
-    assertArrayEquals(expected, classifier.classifyArray(input));
+    Array DATA = Array.makeFromJavaArray(input);
+    assertArrayEquals(expected, classifier.classifyDoubleArray(DATA));
   }
 
   @Test
-  public void testClassifyDoubleArray_AllNegative() throws InvalidValueException {
+  public void testClassifyDoubleArray_AllNegative() {
     Classifier classifier = new Classifier();
     double[] input = {-1.1, -2.2, -3.3};
     int[] expected = {0, 0, 0};
-    assertArrayEquals(expected, classifier.classifyArray(input));
+    Array DATA = Array.makeFromJavaArray(input);
+    assertArrayEquals(expected, classifier.classifyDoubleArray(DATA));
   }
 
   @Test
-  public void testClassifyDoubleArray_Mixed() throws InvalidValueException {
+  public void testClassifyDoubleArray_Mixed() {
     Classifier classifier = new Classifier();
     double[] input = {-1.1, 2.2, -3.3, 4.4};
     int[] expected = {0, 1, 0, 1};
-    assertArrayEquals(expected, classifier.classifyArray(input));
+    Array DATA = Array.makeFromJavaArray(input);
+    assertArrayEquals(expected, classifier.classifyDoubleArray(DATA));
   }
 
   @Test
-  public void testClassifyDoubleArray_WithZero() throws InvalidValueException {
+  public void testClassifyDoubleArray_WithZero() {
     Classifier classifier = new Classifier();
     double[] input = {0.0, -1.1, 1.1, 0.0, 0.0, 0.0};
     int[] expected = {1, 0, 1, 1, 1, 1};
-    assertArrayEquals(expected, classifier.classifyArray(input));
+    Array DATA = Array.makeFromJavaArray(input);
+    assertArrayEquals(expected, classifier.classifyDoubleArray(DATA));
   }
 
-  @Test(expected = InvalidValueException.class)
-  public void testClassifyDoubleArray_WithNaN() throws InvalidValueException {
-    Classifier classifier = new Classifier();
-    double[] input = {Double.NaN, 1.1, -1.1};
-    classifier.classifyArray(input);
-  }
+
 
   /** testing with floats */
   @Test
-  public void testClassifyFloatArray_AllPositive() throws InvalidValueException {
+  public void testClassifyFloatArray_AllPositive() {
     Classifier classifier = new Classifier();
     float[] input = {1.1f, 2.2f, 3.3f};
     int[] expected = {1, 1, 1};
-    assertArrayEquals(expected, classifier.classifyArray(input));
+    Array DATA = Array.makeFromJavaArray(input);
+    assertArrayEquals(expected, classifier.classifyFloatArray(DATA));
   }
 
   @Test
-  public void testClassifyFloatArray_AllNegative() throws InvalidValueException {
+  public void testClassifyFloatArray_AllNegative() {
     Classifier classifier = new Classifier();
     float[] input = {-1.1f, -2.2f, -3.3f};
     int[] expected = {0, 0, 0};
-    assertArrayEquals(expected, classifier.classifyArray(input));
+    Array DATA = Array.makeFromJavaArray(input);
+    assertArrayEquals(expected, classifier.classifyFloatArray(DATA));
   }
 
   @Test
-  public void testClassifyFloatArray_Mixed() throws InvalidValueException {
+  public void testClassifyFloatArray_Mixed() {
     Classifier classifier = new Classifier();
     float[] input = {-1.1f, 2.2f, -3.3f, 4.4f};
     int[] expected = {0, 1, 0, 1};
-    assertArrayEquals(expected, classifier.classifyArray(input));
+    Array DATA = Array.makeFromJavaArray(input);
+    assertArrayEquals(expected, classifier.classifyFloatArray(DATA));
   }
 
   @Test
-  public void testClassifyFloatArray_WithZero() throws InvalidValueException {
+  public void testClassifyFloatArray_WithZero() {
     Classifier classifier = new Classifier();
     float[] input = {0.0f, -1.1f, 1.1f};
     int[] expected = {1, 0, 1};
-    assertArrayEquals(expected, classifier.classifyArray(input));
+    Array DATA = Array.makeFromJavaArray(input);
+    assertArrayEquals(expected, classifier.classifyFloatArray(DATA));
   }
 
-  @Test(expected = InvalidValueException.class)
-  public void testClassifyFloatArray_WithNaN() throws InvalidValueException {
-    Classifier classifier = new Classifier();
-    float[] input = {Float.NaN, 1.1f, -1.1f};
-    classifier.classifyArray(input);
-  }
 
 
 }
