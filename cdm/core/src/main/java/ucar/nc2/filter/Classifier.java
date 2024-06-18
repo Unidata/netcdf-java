26eaf7113137e7d2ec88d2860e889a03c80dad8a
Leo <matakleo@gmail.com>
2024-06-18 13:03:33 -0600

    starting to work

--- cdm/core/src/main/java/ucar/nc2/filter/Classifier.java
+++ cdm/core/src/main/java/ucar/nc2/filter/Classifier.java
@@ -4,22 +4,70 @@ import java.io.IOException;
 import ucar.ma2.Array;
 import ucar.ma2.DataType;
 import ucar.ma2.IndexIterator;
+import ucar.nc2.NetcdfFile;
+import ucar.nc2.Variable;
+import ucar.nc2.dataset.NetcdfDatasets;
 import ucar.nc2.dataset.VariableDS;
+import ucar.nc2.Attribute;
+import ucar.nc2.dataset.NetcdfDatasets;
+import ucar.nc2.constants.CDM;
+import java.nio.*;
+import java.util.HashMap;
+import java.util.Map;
+import ucar.nc2.Attribute;
+import ucar.nc2.Variable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Arrays;
+
+import static ucar.ma2.DataType.*;
+import static ucar.ma2.MAMath.nearlyEquals;
 
 public class Classifier implements Enhancement {
   private Classifier classifier = null;
   private static Classifier emptyClassifier;
+  private static Classifier specidifedClassifier;
   private int classifiedVal;
+  private static int CatStart;
+  private static int CatEnd;
+  private static int CatLabel;
   private int[] classifiedArray;
+  private List<Attribute> AttCat;
+  private List<int[]> rules = new ArrayList<>();
 
   public static Classifier createFromVariable(VariableDS var) {
     try {
       Array arr = var.read();
-      // DataType type = var.getDataType();
+//      // DataType type = var.getDataType();
+      Attribute classifier_config = var.findAttribute(CDM.RANGE_CAT);
+      if (classifier_config != null && !classifier_config.isString()) {
+        System.out.println(classifier_config.getNumericValue());
+
+//        var.remove(scaleAtt);
+      }
       return emptyClassifier();
     } catch (IOException e) {
       return emptyClassifier();
     }
+  }
+
+  public static Classifier createFromVariable(Variable var) {
+    try {
+      Array arr = var.read();
+      CatStart = arr.getInt(0);
+      CatEnd = arr.getInt(1);
+      CatLabel = arr.getInt(2);
+
+      return emptyClassifier();
+    } catch (IOException e) {
+      return emptyClassifier();
+    }
+  }
+
+  public static Classifier ClassifierWithLabel(int CatStart, int CatEnd, int Label) {
+    specidifedClassifier = new Classifier();
+    return emptyClassifier;
   }
 
   public static Classifier emptyClassifier() {
@@ -29,6 +77,26 @@ public class Classifier implements Enhancement {
 
   /** Enough of a constructor */
   public Classifier() {}
+  public Classifier(List<Attribute> AttCat) {
+    this.AttCat = AttCat;
+    this.rules = loadClassificationRules();
+
+  }
+
+  public int[] classifyWithAttributes (Array arr){
+    int[] classifiedArray = new int[(int) arr.getSize()];
+    IndexIterator iterArr = arr.getIndexIterator();
+    int i =0;
+    while (iterArr.hasNext()) {
+      Number value = (Number) iterArr.getObjectNext();
+      if (!Double.isNaN(value.doubleValue())) {
+        classifiedArray[i] = classifyArrayAttribute(value.doubleValue(),this.rules);
+      }
+      i++;
+    }
+    return classifiedArray;
+  }
+
 
   /** Classify double array */
   public int[] classifyDoubleArray(Array arr) {
@@ -48,6 +116,8 @@ public class Classifier implements Enhancement {
 
 
 
+
+
   /** for a single double */
   public int classifyArray(double val) {
     if (val >= 0) {
@@ -59,10 +129,61 @@ public class Classifier implements Enhancement {
     return classifiedVal;
   }
 
+  public int classifyArrayAttribute(double val, List<int[]> rules) {
+      for (int[] rule : rules) {
+        if (val > rule[0] && val <= rule[1]) {
+          val = rule[2];
+        }
+
+    }
+    return (int) val;
+
+  }
+
+  // Method to load classification rules from the attributes
+  public List<int[]> loadClassificationRules() {
+    for (Attribute attribute : this.AttCat) {
+      int[] rule = stringToIntArray(attribute.getStringValue());
+      this.rules.add(rule);
+    }
+    return rules;
+  }
+
   @Override
   public double convert(double val) {
     return emptyClassifier.classifyArray(val);
   }
+  public static int[] stringToIntArray(String str) {
+    String[] stringArray = str.split(" "); // Split the string by spaces
+    int[] intArray = new int[stringArray.length]; // Create an array to hold the parsed integers
+
+    for (int i = 0; i < stringArray.length; i++) {
+      intArray[i] = Integer.parseInt(stringArray[i]); // Parse each string to an integer
+    }
+
+    return intArray;
+  }
+  public static void main(String[] args) {
+
+ String dataDir = "/Users/lmatak/Desktop/netCDF-Java/netcdf-java/cdm/core/src/test/data/ncml/enhance/";
+
+    try (NetcdfFile ncfile = NetcdfDatasets.openDataset(dataDir + "testAddToClassifier.ncml", true, null)) {
+
+      Variable Classify_Specsx = ncfile.findVariable("class_specs");
+      Array Data = Classify_Specsx.read();
+      List<Attribute> Whatever = Classify_Specsx.getAttributes();
+      Classifier classifier = new Classifier(Whatever);
+      int[] ClassifiedArray = classifier.classifyWithAttributes(Data);
+      // Print the entire array
+      System.out.println(Arrays.toString(ClassifiedArray));
+
+  } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+
+  }
+
 
 
 }
