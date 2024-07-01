da644a8153c4004902243b5786719342a0d44f86
Leo Matak <78696884+matakleo@users.noreply.github.com>
2024-07-01 11:17:03 -0500

    Classifier (#1357)
    * the best version so far
    
    * matche python mostly
    
    * before major change, kinda works
    
    * long one, all numpy stuff
    
    * classfier v1
    
    * Seems to be working
    
    * stlye check
    
    * removed some not used stuff
    
    * add additional convert methods
    
    * some test fixes
    
    * rehaul
    
    * changes from code review comments
    
    * starting to work
    
    * trying new ncml
    
    * new tests
    
    * all seems to work
    
    * all seems to be working!
    
    * applied style
    
    * final style update
    
    * fix the shape of class_specs to 17
    
    * fix pr comments
    
    * fix the flaky test
    
    * delete tests, modify constructor
    
    * remove unused ncml, add int and float test
    
    * update variable length to correct num
    
    * Suppress spurious/not relevant jfreechart CVEs (#1358)
    
    finalize reviews
    
    ---------
    
    Co-authored-by: Tara Drwenski <tdrwenski@users.noreply.github.com>
    Co-authored-by: Hailey Johnson <hajohns@ucar.edu>

--- cdm/core/src/main/java/ucar/nc2/filter/Classifier.java
+++ cdm/core/src/main/java/ucar/nc2/filter/Classifier.java
@@ -1,45 +1,63 @@
 package ucar.nc2.filter;
 
-import java.io.IOException;
+
 import ucar.ma2.Array;
-import ucar.ma2.DataType;
 import ucar.ma2.IndexIterator;
-import ucar.nc2.dataset.VariableDS;
+import ucar.nc2.Variable;
+import ucar.nc2.constants.CDM;
+import ucar.nc2.Attribute;
+import ucar.nc2.util.Misc;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Classifier implements Enhancement {
-  private Classifier classifier = null;
-  private static Classifier emptyClassifier;
-  private int classifiedVal;
-  private int[] classifiedArray;
+  private String[] AttCat;
+  private List<int[]> rules = new ArrayList<>();
 
-  public static Classifier createFromVariable(VariableDS var) {
-    try {
-      Array arr = var.read();
-      // DataType type = var.getDataType();
-      return emptyClassifier();
-    } catch (IOException e) {
-      return emptyClassifier();
+  public Classifier() {
+    this.AttCat = new String[0];
+    this.rules = new ArrayList<>();
+  }
+
+  // Constructor with attributes
+  public Classifier(String[] AttCat) {
+    this.AttCat = AttCat;
+    this.rules = loadClassificationRules();
+  }
+
+  // Factory method to create a Classifier from a Variable
+  public static Classifier createFromVariable(Variable var) {
+    List<Attribute> attributes = var.attributes().getAttributes();
+
+    for (Attribute attribute : attributes) {
+      // check like this, or something else?
+      if (attribute == var.attributes().findAttribute(CDM.CLASSIFY)) {
+        String[] sets = attribute.getStringValue().split(";");
+        for (int i = 0; i < sets.length; i++) {
+          // trim and clean so it's ready
+          sets[i] = sets[i].trim();
+        }
+        return new Classifier(sets);
+      }
     }
+
+    return new Classifier();
+
   }
 
-  public static Classifier emptyClassifier() {
-    emptyClassifier = new Classifier();
-    return emptyClassifier;
-  }
 
-  /** Enough of a constructor */
-  public Classifier() {}
 
-  /** Classify double array */
-  public int[] classifyDoubleArray(Array arr) {
+  public int[] classifyWithAttributes(Array arr) {
     int[] classifiedArray = new int[(int) arr.getSize()];
-    int i = 0;
     IndexIterator iterArr = arr.getIndexIterator();
+    int i = 0;
     while (iterArr.hasNext()) {
       Number value = (Number) iterArr.getObjectNext();
       if (!Double.isNaN(value.doubleValue())) {
-
-        classifiedArray[i] = classifyArray(value.doubleValue());
+        classifiedArray[i] = classifyArrayAttribute(value.doubleValue());
+      } else {
+        classifiedArray[i] = Integer.MIN_VALUE;
       }
       i++;
     }
@@ -48,23 +66,54 @@ public class Classifier implements Enhancement {
 
 
 
-  /** for a single double */
-  public int classifyArray(double val) {
-    if (val >= 0) {
-      classifiedVal = 1;
-    } else {
-      classifiedVal = 0;
+  public int classifyArrayAttribute(double val) {
+    for (int[] rule : rules) {
+      if (val > rule[0] && val <= rule[1] + Misc.defaultMaxRelativeDiffFloat) {
+        return rule[2];
+      }
     }
+    // Return min possible int if no rule matches
+    return Integer.MIN_VALUE;
+  }
 
-    return classifiedVal;
+  // Method to load classification rules from the attributes
+  private List<int[]> loadClassificationRules() {
+    for (String rules : this.AttCat) {
+      int[] rule = stringToIntArray(rules);
+      this.rules.add(rule);
+    }
+    return rules;
   }
 
   @Override
   public double convert(double val) {
-    return emptyClassifier.classifyArray(val);
+    return classifyArrayAttribute(val);
   }
 
+  public static int[] stringToIntArray(String str) {
+    String[] stringArray = str.split(" "); // Split the string by spaces
+    int[] intArray = new int[stringArray.length]; // Create an array to hold the parsed integers
+
+    for (int i = 0; i < stringArray.length; i++) {
+
+      double value = Double.parseDouble(stringArray[i]); // Parse each string to a double
+
+      if (Double.isNaN(value)) {
+        // Check if the entry is NaN and assign Integer.MIN_VALUE or Integer.MAX_VALUE based on the index
+        if (i == 0) {
+          intArray[i] = Integer.MIN_VALUE;
+        } else if (i == 1) {
+          intArray[i] = Integer.MAX_VALUE;
+        } else {
+          intArray[i] = -99999; // Default value for other indices if needed
+        }
+      } else {
+        intArray[i] = (int) value; // Convert the value to int if it is not NaN
+      }
+
+    }
+
+    return intArray;
+  }
 
 }
-
-
--- cdm/core/src/test/java/ucar/nc2/ncml/TestEnhanceClassifier.java
+++ cdm/core/src/test/java/ucar/nc2/ncml/TestEnhanceClassifier.java
@@ -2,114 +2,86 @@ package ucar.nc2.ncml;
 
 import static com.google.common.truth.Truth.assertThat;
 import static ucar.ma2.MAMath.nearlyEquals;
-
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.Test;
 import ucar.ma2.Array;
 import ucar.ma2.DataType;
+import ucar.nc2.Attribute;
 import ucar.nc2.NetcdfFile;
 import ucar.nc2.Variable;
 import ucar.nc2.dataset.NetcdfDatasets;
+import ucar.nc2.filter.Classifier;
 import ucar.unidata.util.test.TestDir;
 
 public class TestEnhanceClassifier {
 
   private static String dataDir = TestDir.cdmLocalTestDataDir + "ncml/enhance/";
-
-  public static final int[] all_ones = {1, 1, 1, 1, 1};
-  public static final Array DATA_all_ones = Array.makeFromJavaArray(all_ones);
-  public static final int[] all_zeroes = {0, 0, 0, 0, 0};
-  public static final Array DATA_all_zeroes = Array.makeFromJavaArray(all_zeroes);
-  public static final int[] mixNumbers = {1, 0, 1, 1, 0};
+  public static final int[] mixNumbers = {1, 0, 0, 1, 0};
   public static final Array DATA_mixNumbers = Array.makeFromJavaArray(mixNumbers);
+  public static final int[] Classification_test =
+      {0, -2147483648, 0, 0, 10, 10, 10, 100, 100, 100, -2147483648, 100, 1000, 1000, 1000, 1000, 1000};
+  public static final Array CLASSIFICATION_TEST = Array.makeFromJavaArray(Classification_test);
 
-
-  /** test on doubles, all positives, all negatives and a mixed array */
   @Test
-  public void testEnhanceClassifier_doubles() throws IOException {
-    try (NetcdfFile ncfile = NetcdfDatasets.openDataset(dataDir + "testClassifier.ncml", true, null)) {
-      Variable doublePositives = ncfile.findVariable("doublePositives");
-      assertThat((Object) doublePositives).isNotNull();
-      assertThat(doublePositives.getDataType()).isEqualTo(DataType.DOUBLE);
-      assertThat(doublePositives.attributes().hasAttribute("classify")).isTrue();
-      Array dataDoubles = doublePositives.read();
-      assertThat(nearlyEquals(dataDoubles, DATA_all_ones)).isTrue();
+  public void testEnhanceClassifier_Ints() throws IOException {
 
-      Variable doubleNegatives = ncfile.findVariable("doubleNegatives");
-      assertThat((Object) doubleNegatives).isNotNull();
-      assertThat(doubleNegatives.getDataType()).isEqualTo(DataType.DOUBLE);
-      assertThat(doubleNegatives.attributes().hasAttribute("classify")).isTrue();
-      Array datadoubleNegatives = doubleNegatives.read();
-      assertThat(nearlyEquals(datadoubleNegatives, DATA_all_zeroes)).isTrue();
+    try (NetcdfFile ncfile = NetcdfDatasets.openDataset(dataDir + "testAddToClassifier.ncml", true, null)) {
 
-      Variable doubleMix = ncfile.findVariable("doubleMix");
-      assertThat((Object) doubleMix).isNotNull();
-      assertThat(doubleMix.getDataType()).isEqualTo(DataType.DOUBLE);
-      assertThat(doubleMix.attributes().hasAttribute("classify")).isTrue();
-      Array datadoubleMix = doubleMix.read();
-      assertThat(nearlyEquals(datadoubleMix, DATA_mixNumbers)).isTrue();
-
-    }
+      Variable classifySpecs = ncfile.findVariable("classify_ints");
+      assertThat((Object) classifySpecs).isNotNull();
+      assertThat(!classifySpecs.attributes().isEmpty()).isTrue();
+      Array Data = classifySpecs.read();
+      Classifier classifier = Classifier.createFromVariable(classifySpecs);
+      int[] ClassifiedArray = classifier.classifyWithAttributes(Data);
+      assertThat(nearlyEquals(Array.makeFromJavaArray(ClassifiedArray), DATA_mixNumbers)).isTrue();
 
 
-  }
-
-  /** test on floats, all positives, all negatives and a mixed array */
-  @Test
-  public void testEnhanceClassifier_floats() throws IOException {
-    try (NetcdfFile ncfile = NetcdfDatasets.openDataset(dataDir + "testClassifier.ncml", true, null)) {
-
-      Variable floatPositives = ncfile.findVariable("floatPositives");
-      assertThat((Object) floatPositives).isNotNull();
-      assertThat(floatPositives.getDataType()).isEqualTo(DataType.FLOAT);
-      assertThat(floatPositives.attributes().hasAttribute("classify")).isTrue();
-      Array datafloats = floatPositives.read();
-      assertThat(nearlyEquals(datafloats, DATA_all_ones)).isTrue();
-
-      Variable floatNegatives = ncfile.findVariable("floatNegatives");
-      assertThat((Object) floatNegatives).isNotNull();
-      assertThat(floatNegatives.getDataType()).isEqualTo(DataType.FLOAT);
-      assertThat(floatNegatives.attributes().hasAttribute("classify")).isTrue();
-      Array datafloatNegatives = floatNegatives.read();
-      assertThat(nearlyEquals(datafloatNegatives, DATA_all_zeroes)).isTrue();
-
-      Variable floatMix = ncfile.findVariable("floatMix");
-      assertThat((Object) floatMix).isNotNull();
-      assertThat(floatMix.getDataType()).isEqualTo(DataType.FLOAT);
-      assertThat(floatMix.attributes().hasAttribute("classify")).isTrue();
-      Array datafloatsMix = floatMix.read();
-      assertThat(nearlyEquals(datafloatsMix, DATA_mixNumbers)).isTrue();
-
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
 
   }
 
-  /** enhance is not applied to Integers, so we expect the same values after application */
   @Test
-  public void testEnhanceClassifier_integers() throws IOException {
+  public void testEnhanceClassifier_Floats() throws IOException {
 
-    try (NetcdfFile ncfile = NetcdfDatasets.openDataset(dataDir + "testClassifier.ncml", true, null)) {
-      Variable IntegerPositives = ncfile.findVariable("intPositives");
-      assertThat((Object) IntegerPositives).isNotNull();
-      assertThat(IntegerPositives.getDataType()).isEqualTo(DataType.INT);
-      assertThat(IntegerPositives.attributes().hasAttribute("classify")).isTrue();
-      Array dataIntegers = IntegerPositives.read();
-      assertThat(nearlyEquals(dataIntegers, DATA_all_ones)).isTrue();
+    try (NetcdfFile ncfile = NetcdfDatasets.openDataset(dataDir + "testAddToClassifier.ncml", true, null)) {
 
-      Variable intNegatives = ncfile.findVariable("intNegatives");
-      assertThat((Object) intNegatives).isNotNull();
-      assertThat(intNegatives.getDataType()).isEqualTo(DataType.INT);
-      assertThat(intNegatives.attributes().hasAttribute("classify")).isTrue();
-      Array dataintNegatives = intNegatives.read();
-      assertThat(nearlyEquals(dataintNegatives, DATA_all_zeroes)).isTrue();
+      Variable classifySpecs = ncfile.findVariable("classify_floats");
+      assertThat((Object) classifySpecs).isNotNull();
+      assertThat(!classifySpecs.attributes().isEmpty()).isTrue();
+      Array Data = classifySpecs.read();
+      Classifier classifier = Classifier.createFromVariable(classifySpecs);
+      int[] ClassifiedArray = classifier.classifyWithAttributes(Data);
+      assertThat(nearlyEquals(Array.makeFromJavaArray(ClassifiedArray), DATA_mixNumbers)).isTrue();
 
-      Variable intMix = ncfile.findVariable("intMix");
-      assertThat((Object) intMix).isNotNull();
-      assertThat(intMix.getDataType()).isEqualTo(DataType.INT);
-      assertThat(intMix.attributes().hasAttribute("classify")).isTrue();
-      Array dataintMix = intMix.read();
-      assertThat(nearlyEquals(dataintMix, DATA_mixNumbers)).isTrue();
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
 
   }
+
+  @Test
+  public void testEnhanceClassifier_classification() throws IOException {
+
+    try (NetcdfFile ncfile = NetcdfDatasets.openDataset(dataDir + "testAddToClassifier.ncml", true, null)) {
+
+      Variable classifySpecs = ncfile.findVariable("class_specs");
+      assertThat((Object) classifySpecs).isNotNull();
+      assertThat(!classifySpecs.attributes().isEmpty()).isTrue();
+      Array Data = classifySpecs.read();
+      Classifier classifier = Classifier.createFromVariable(classifySpecs);
+      int[] ClassifiedArray = classifier.classifyWithAttributes(Data);
+      assertThat(nearlyEquals(Array.makeFromJavaArray(ClassifiedArray), CLASSIFICATION_TEST)).isTrue();
+
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+
 }
