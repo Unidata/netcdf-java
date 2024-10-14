431a142d9787db3d9690bfafe14407def8568e4c
haileyajohnson <haj022@ucsd.edu>
2024-10-14 14:38:30 -0700

    whitespace changes/force rerun of tests

--- cdm/core/src/main/java/ucar/nc2/filter/Classifier.java
+++ cdm/core/src/main/java/ucar/nc2/filter/Classifier.java
@@ -17,7 +17,6 @@ import ucar.nc2.*;
 
 public class Classifier implements Enhancement {
 
-
   private String[] AttCat;
   private List<int[]> rules = new ArrayList<>();
 
@@ -36,7 +35,6 @@ public class Classifier implements Enhancement {
   }
 
   // Factory method to create a Classifier from a Variable
-
   public static Classifier createFromVariable(VariableDS var) {
     List<Attribute> attributes = var.attributes().getAttributes();
 
@@ -53,10 +51,7 @@ public class Classifier implements Enhancement {
     }
 
     return new Classifier();
-
   }
-
-
 
   public int[] classifyWithAttributes(Array arr) {
     int[] classifiedArray = new int[(int) arr.getSize()];
@@ -73,8 +68,6 @@ public class Classifier implements Enhancement {
     }
     return classifiedArray;
   }
-
-
 
   public int classifyArrayAttribute(double val) {
     for (int[] rule : rules) {
